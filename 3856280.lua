1923a27a82a67f6e3290f15032e3b2b1db4b89b7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-01 10:10:17 +0700

    Added manifests for AppID 3856280

---
+++ 3856280.lua
@@ -0,0 +1,14 @@
+-- 3856280's Lua and Manifest Created by Hubcap Manifest
+-- holoVillage: Our Cozy Days
+-- Created: April 29, 2026 at 06:46:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3856280) -- holoVillage: Our Cozy Days
+-- MAIN APP DEPOTS
+addappid(3856281, 1, "dd5dafbfa6d1302c7f7028e7aa17faba71d57b0b38aecaeaf14a23553904ad77") -- Depot 3856281
+setManifestid(3856281, "888894819048595051", 6653193617)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4582350) --  DLC - 3
