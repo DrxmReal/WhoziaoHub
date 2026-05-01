bdf70fbd17027a09e3a6cb3fb6ea9a3c8747a9e4
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-01 10:10:42 +0700

    Added manifests for AppID 4225940

---
+++ 4225940.lua
@@ -0,0 +1,12 @@
+-- 4225940's Lua and Manifest Created by Hubcap Manifest
+-- 不好啦！师父被女妖精抓走了！
+-- Created: April 15, 2026 at 02:47:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4225940, 1, "c9aeff608feb6928acd22bde046e47615048be1ba4cd5c1799db4ed0da8c0a7c") -- 不好啦！师父被女妖精抓走了！
+-- MAIN APP DEPOTS
+addappid(4225941, 1, "d895f760564551ea206a8fddc5d80ec3cf5048493fe6ffaeb31e03cb611ea959") -- Depot 4225941
+setManifestid(4225941, "3152334534204867942", 55907654981)
