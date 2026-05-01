dabda4b127b1d0297ef32e8e0e5b5e7f7a316289
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-01 14:34:49 +0700

    Add LaVonne's Bouncing Idle via Bot

---
+++ 4606730.lua
@@ -0,0 +1,12 @@
+-- 4606730's Lua and Manifest Created by Hubcap Manifest
+-- LaVonne's Bouncing Idle
+-- Created: May 01, 2026 at 03:24:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4606730) -- LaVonne's Bouncing Idle
+-- MAIN APP DEPOTS
+addappid(4606731, 1, "6d0e5a430353f68c0099f9934ba4697dc74e6ce53048ac1e71efa8af6e5fd7f9") -- Depot 4606731
+setManifestid(4606731, "3936473207402805885", 418661936)
