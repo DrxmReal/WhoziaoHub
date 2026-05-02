f3052b8bc06cc8642d09ab7a3f79d5885372be0b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-02 14:56:41 +0700

    Tai moi Distant Oceanic Getaway via Bot

---
+++ 2861290.lua
@@ -0,0 +1,14 @@
+-- 2861290's Lua and Manifest Created by Hubcap Manifest
+-- Distant Oceanic Getaway
+-- Created: May 01, 2026 at 07:13:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2861290) -- Distant Oceanic Getaway
+-- MAIN APP DEPOTS
+addappid(2861292, 1, "57c7abdba5e17486f26a0589d226eb82064028f9b91388ece544ce6f94d1d654") -- Depot 2861292
+setManifestid(2861292, "5479710376123071839", 1265770782)
+addappid(2861293, 1, "6bb679ea2e0e15f6978c8076e2f94bdf40339f03ef1c5fc6380b4e93fc3cb6ae") -- Depot 2861293
+setManifestid(2861293, "2495726588157999254", 1248868133)
