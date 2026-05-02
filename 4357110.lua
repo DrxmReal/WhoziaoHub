82e83a4aef3b5598f0f16f97b05400f747b12f1d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-02 14:19:47 +0700

    Tải mới Japanese Mature: Sex Dreams via Bot

---
+++ 4357110.lua
@@ -0,0 +1,12 @@
+-- 4357110's Lua and Manifest Created by Hubcap Manifest
+-- Japanese Mature: Sex Dreams
+-- Created: May 01, 2026 at 16:05:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4357110) -- Japanese Mature: Sex Dreams
+-- MAIN APP DEPOTS
+addappid(4357111, 1, "79c122b2e6be671b1e13735af7f79780685fbc3da83a7b1a478aa17e30048505") -- Depot 4357111
+setManifestid(4357111, "1190675128748766470", 11821946559)
