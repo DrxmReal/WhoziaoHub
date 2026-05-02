a5961c5cac9a7d00aaf21aad2c26fa04640d31d2
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-02 15:11:24 +0700

    Tai moi Goblin Vyke: The Thief Tycoon via Bot

---
+++ 3794610.lua
@@ -0,0 +1,12 @@
+-- 3794610's Lua and Manifest Created by Hubcap Manifest
+-- Goblin Vyke: The Thief Tycoon
+-- Created: May 02, 2026 at 03:31:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3794610) -- Goblin Vyke: The Thief Tycoon
+-- MAIN APP DEPOTS
+addappid(3794611, 1, "16a6e2e09b7a3eab21f78850c640210ad507609f1e661af8d089dcc49eca8742") -- Depot 3794611
+setManifestid(3794611, "3544966615965260814", 1936425437)
