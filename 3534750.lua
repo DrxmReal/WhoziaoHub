b6f0b98cd68e39481c1288c73a05ffb1e41e1538
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-02 14:36:08 +0700

    Tai moi Descent Into Darkness via Bot

---
+++ 3534750.lua
@@ -0,0 +1,12 @@
+-- 3534750's Lua and Manifest Created by Hubcap Manifest
+-- Descent Into Darkness
+-- Created: May 01, 2026 at 07:15:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3534750) -- Descent Into Darkness
+-- MAIN APP DEPOTS
+addappid(3534751, 1, "ee5a3dba0dd039907d3b24f89f61f191d776461cdf031bb53e4d85d2e1cecb41") -- Depot 3534751
+setManifestid(3534751, "2025257836446832764", 3058223766)
