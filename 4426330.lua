f62c172eed9686a119e632f3382cf4b8b48ea489
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-02 14:06:44 +0700

    Tải mới Slime Kingdom: Idle via Bot

---
+++ 4426330.lua
@@ -0,0 +1,12 @@
+-- 4426330's Lua and Manifest Created by Hubcap Manifest
+-- Slime Kingdom: Idle
+-- Created: May 01, 2026 at 19:33:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4426330) -- Slime Kingdom: Idle
+-- MAIN APP DEPOTS
+addappid(4426331, 1, "f26cd6f0de62032959d4d7ddb597679e00490a3c1ed86d2f76207e3bc3113f5a") -- Depot 4426331
+setManifestid(4426331, "5092016692836977251", 486266327)
