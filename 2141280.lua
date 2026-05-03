651b9bb3eae2a3459165a472cd5eb9394ee72aee
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 12:28:12 +0700

    Tai moi GladiEATers via Bot

---
+++ 2141280.lua
@@ -0,0 +1,12 @@
+-- 2141280's Lua and Manifest Created by Hubcap Manifest
+-- GladiEATers
+-- Created: May 02, 2026 at 23:27:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2141280) -- GladiEATers
+-- MAIN APP DEPOTS
+addappid(2141281, 1, "3298549a9e4072332ca8281eea8f7339da48d6cc4735a635485a038c959e750e") -- Depot 2141281
+setManifestid(2141281, "6885450425672876114", 1311087530)
