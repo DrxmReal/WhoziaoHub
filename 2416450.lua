037ced1f11c9d3734d31b6c70c2938e85e6e44f6
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 22:10:21 +0700

    Tai moi MOUSE: P.I. For Hire via Bot

---
+++ 2416450.lua
@@ -0,0 +1,26 @@
+-- 2416450's Lua and Manifest Created by Hubcap Manifest
+-- MOUSE: P.I. For Hire
+-- Created: April 24, 2026 at 21:50:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2416450, 1, "56df9203dd3706f1816684fd80aec6fd14bacaf541f8274f7d84eb9ae924b04e") -- MOUSE: P.I. For Hire
+-- MAIN APP DEPOTS
+addappid(2416451, 1, "c52f8369321c8357293962eacf7c7f416c2ea912c3d2640703880a12c964732a") -- Depot 2416451
+setManifestid(2416451, "5937011685208962632", 10558365458)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- MOUSE P.I. For Hire Comic Book (AppID: 3879490)
+addappid(3879490)
+addappid(3879491, 1, "a609219532d6690630d1cccd2eb190817cca2d15d8380c0d3ba2bb1d91108f67") -- MOUSE P.I. For Hire Comic Book - Depot 3879491
+setManifestid(3879491, "6434545354655347775", 6269067)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(3912890) -- MOUSE: P.I. For Hire Story DLC (unreleased)
