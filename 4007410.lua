fa138d5d2b15ed84cd2f8f3cc60165874ad4505d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 12:29:07 +0700

    Tai moi Ved:Recure Demo via Bot

---
+++ 4007410.lua
@@ -0,0 +1,12 @@
+-- 4007410's Lua and Manifest Created by Hubcap Manifest
+-- Ved:疗愈所 Demo
+-- Created: May 02, 2026 at 23:26:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4007410) -- Ved:疗愈所 Demo
+-- MAIN APP DEPOTS
+addappid(4007411, 1, "3dee7529267d11eb2c42af18db69b7176bd2b57294349ae41d982c7514489081") -- Depot 4007411
+setManifestid(4007411, "7756598833604780246", 15647846575)
