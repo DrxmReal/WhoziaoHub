e597eaa471327438977522423c1b435502aa0b85
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 12:29:43 +0700

    Tai moi 栖云：遗忘之境 Oriental Dreams : The Forgotten Realm Demo via Bot

---
+++ 3745210.lua
@@ -0,0 +1,12 @@
+-- 3745210's Lua and Manifest Created by Hubcap Manifest
+-- 栖云：遗忘之境 Oriental Dreams : The Forgotten Realm Demo
+-- Created: May 02, 2026 at 23:25:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3745210) -- 栖云：遗忘之境 Oriental Dreams : The Forgotten Realm Demo
+-- MAIN APP DEPOTS
+addappid(3745211, 1, "e6af21c16a44ed41f2357855fafc0faec911429c6957a35c328a425364374758") -- Depot 3745211
+setManifestid(3745211, "221177006206377137", 891672633)
