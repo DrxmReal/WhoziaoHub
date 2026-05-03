7b83cfffa55999d50858b28fab32d601df88e01c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 22:08:11 +0700

    Tai moi FUTA FIGHT & FUCK 🥊 via Bot

---
+++ 4491210.lua
@@ -0,0 +1,12 @@
+-- 4491210's Lua and Manifest Created by Hubcap Manifest
+-- FUTA FIGHT & FUCK 🥊
+-- Created: May 02, 2026 at 15:24:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4491210) -- FUTA FIGHT & FUCK 🥊
+-- MAIN APP DEPOTS
+addappid(4491211, 1, "701bd7ff1d125359ba89f8bab04d6834adb229be93d8bafc590b90e483fe98df") -- Depot 4491211
+setManifestid(4491211, "764498474340875291", 1272440470)
