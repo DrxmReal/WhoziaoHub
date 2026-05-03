67ff0de4dada89daf5b411d3a297b71b6e80945d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 12:28:34 +0700

    Tai moi 1325 Maple Lane via Bot

---
+++ 3456620.lua
@@ -0,0 +1,12 @@
+-- 3456620's Lua and Manifest Created by Hubcap Manifest
+-- 1325 Maple Lane
+-- Created: May 02, 2026 at 23:26:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3456620) -- 1325 Maple Lane
+-- MAIN APP DEPOTS
+addappid(3456622, 1, "2983f87f0689bc0f76c2e329facfbab57acbb6203311a7e1ea834ca378674a31") -- Depot 3456622
+setManifestid(3456622, "5899583752307916474", 2856570706)
