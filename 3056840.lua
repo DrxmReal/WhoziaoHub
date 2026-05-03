c428929b0a35298fcf828e72461e99fe97861e06
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-03 22:06:48 +0700

    Tai moi A Bumpy Ride via Bot

---
+++ 3056840.lua
@@ -0,0 +1,18 @@
+-- 3056840's Lua and Manifest Created by Hubcap Manifest
+-- A Bumpy Ride
+-- Created: May 03, 2026 at 01:34:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3056840) -- A Bumpy Ride
+-- MAIN APP DEPOTS
+addappid(3056841, 1, "2cc0603f6e4a27a2fd20f8b3c72df3fe0e22651b0bf1ccafa973db5327cbd46d") -- Depot 3056841
+setManifestid(3056841, "8228147367928844691", 1952297842)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
