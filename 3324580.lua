33ee70597e2b865bff8ff0fd543a17b275b6119a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-04 11:55:22 +0700

    Tai moi Planet Lust via Bot

---
+++ 3324580.lua
@@ -0,0 +1,16 @@
+-- 3324580's Lua and Manifest Created by Hubcap Manifest
+-- Planet Lust
+-- Created: April 28, 2026 at 16:51:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3324580) -- Planet Lust
+-- MAIN APP DEPOTS
+addappid(3324581, 1, "9169eb5e666a306bc434fea0915dfb2b344fcae5bfccf367f37808a85557641f") -- Depot 3324581
+setManifestid(3324581, "4506269262797621625", 207109812)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
