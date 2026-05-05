916eae5935e05cbc17e56e225772090a657fe03a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-05 22:39:01 +0700

    Tai moi Anomalous Coffee Machine 2 via Bot

---
+++ 3797340.lua
@@ -0,0 +1,17 @@
+-- 3797340's Lua and Manifest Created by Hubcap Manifest
+-- Anomalous Coffee Machine 2
+-- Created: May 04, 2026 at 22:14:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3797340, 1, "546502d78e9ee7f5a1c6c8ad576987a712b85981adbaa23a0bdd694dc3197870") -- Anomalous Coffee Machine 2
+-- MAIN APP DEPOTS
+addappid(3797341, 1, "817798feb3ccb1755b4d2175aadb9d3216fa1b79ab93b78223a310ad2e7fa129") -- Depot 3797341
+setManifestid(3797341, "6835817147652032142", 1689510832)
+-- DLCS WITH DEDICATED DEPOTS
+-- Anomalous Coffee Machine 2 - NSFW (AppID: 3875100)
+addappid(3875100)
+addappid(3875100, 1, "9d0f323d7f3c0b74ea236aa04550242e23dc01af62ceee3c6bfacd5c5916aa51") -- Anomalous Coffee Machine 2 - NSFW - Depot 3875100
+setManifestid(3875100, "6032141532889246610", 512172528)
