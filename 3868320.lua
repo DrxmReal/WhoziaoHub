0f397605ead6a3de9002d37f9a016e1c42256c25
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-07 05:54:47 +0700

    Tai moi Forage Wizard via Bot

---
+++ 3868320.lua
@@ -0,0 +1,12 @@
+-- 3868320's Lua and Manifest Created by Hubcap Manifest
+-- Forage Wizard
+-- Created: May 06, 2026 at 08:49:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3868320) -- Forage Wizard
+-- MAIN APP DEPOTS
+addappid(3868323, 1, "9e28b51aa4740d06ca3b37fe73b23b365a51c318798a53a4bd622d73b76e476d") -- Depot 3868323
+setManifestid(3868323, "7156243290955985386", 726375272)
