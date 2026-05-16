26ee535c057c43ce6c66ca44f594e5032d0293c4
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-16 15:45:39 +0700

    Tai moi SiNiSistar 2 via Bot

---
+++ 4466620.lua
@@ -0,0 +1,14 @@
+-- 4466620's Lua and Manifest Created by Hubcap Manifest
+-- SiNiSistar 2
+-- Created: May 16, 2026 at 02:18:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4466620, 1, "de887f3565af8aa5f3b2cfbf3cecc87912c521ae8e8bd441eafd7d8693715564") -- SiNiSistar 2
+-- MAIN APP DEPOTS
+addappid(4466621, 1, "240a8767c6fcb561914f7c4495d7e5a9bf4dc0a2793a40d75351764b1eafa2f6") -- Depot 4466621
+setManifestid(4466621, "5228144781009782201", 1908153256)
+addappid(4466622, 1, "a1afb0bb838f24c8e1b9dd2d8a242c5f9f60f6c3ada589ff1020e8542b06a2d7") -- Depot 4466622
+setManifestid(4466622, "7261781803278655806", 2146942675)
