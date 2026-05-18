2b4c454c862220f74ebec3deaad3215f039fec88
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-18 07:09:32 +0700

    Tai moi The Caribou Trail via Bot

---
+++ 3065030.lua
@@ -0,0 +1,18 @@
+-- 3065030's Lua and Manifest Created by Hubcap Manifest
+-- The Caribou Trail
+-- Created: May 16, 2026 at 02:17:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3065030, 1, "5c15c77d2945ed12c7ba3f5c04cff65360ccb1421a2997ef90ac6903c00b8099") -- The Caribou Trail
+-- MAIN APP DEPOTS
+addappid(3065031, 1, "cda80b7fb0be55d0281a34c83702b30bcbbffac3dddd9b64992d92703fc106ed") -- Depot 3065031
+setManifestid(3065031, "2373728414836523179", 7957848404)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
