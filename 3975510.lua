ad5c2f3dc05d0a06ac7001e51a51eaca5d8b9b69
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-18 20:13:35 +0700

    Tai moi CYAN BRAIN via Bot

---
+++ 3975510.lua
@@ -0,0 +1,16 @@
+-- 3975510's Lua and Manifest Created by Hubcap Manifest
+-- CYAN BRAIN
+-- Created: January 26, 2026 at 07:08:07 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3975510) -- CYAN BRAIN
+-- MAIN APP DEPOTS
+addappid(3975511, 1, "a1bbbcf2b8771f1c4c286a07e143de31eba09b74be882c33eb28c9b41a1fbc4f") -- Depot 3975511
+setManifestid(3975511, "1877905708940531969", 1735347913)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- CYAN BRAIN - Artbook (AppID: 4218650) - missing depot keys
+-- addappid(4218650)
