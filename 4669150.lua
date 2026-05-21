2737b7db35799061a5b0d21e3082ca0448ed7b16
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-21 19:36:47 +0700

    Tai moi Against The Light via Bot

---
+++ 4669150.lua
@@ -0,0 +1,12 @@
+-- 4669150's Lua and Manifest Created by Hubcap Manifest
+-- Against The Light
+-- Created: May 21, 2026 at 06:46:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4669150, 1, "611e75c7756de986fbe3b2dceb73887852470a524296f87daf8baa0225a2becf") -- Against The Light
+-- MAIN APP DEPOTS
+addappid(4669152, 1, "9248836a8805d08e32df9814dfb30c3cf2d488e0cf71c919962b8946a6b60db9") -- Depot 4669152
+setManifestid(4669152, "6252551192397667052", 182628217)
