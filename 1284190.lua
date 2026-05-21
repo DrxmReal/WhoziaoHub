b9125cbb1d1b330dcb1781b4876476d4a497526f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-21 19:22:40 +0700

    Tai moi The Planet Crafter via Bot

---
+++ 1284190.lua
@@ -0,0 +1,15 @@
+-- 1284190's Lua and Manifest Created by Hubcap Manifest
+-- The Planet Crafter
+-- Created: April 29, 2026 at 09:45:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1284190, 1, "d691e043b9b43765cb9dce454231965e93fa7fd5b28ca5c6cfb72af0e785adf9") -- The Planet Crafter
+-- MAIN APP DEPOTS
+addappid(1284192, 1, "1f10eb10bfc54e336b5f2e2a5c05001817700491d0c79a7931637a9158c41dc0") -- Depot 1284192
+setManifestid(1284192, "7141701373859619618", 5701854712)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3142540) -- The Planet Crafter - Planet Humble
+addappid(4078590) -- The Planet Crafter - Toxicity
