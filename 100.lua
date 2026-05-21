bc2f063c2c568bc58aaf424ff4deb38465d11d36
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-21 19:06:33 +0700

    Tai moi Counter-Strike: Condition Zero via Bot

---
+++ 100.lua
@@ -0,0 +1,58 @@
+-- 100's Lua and Manifest Created by Hubcap Manifest
+-- Counter-Strike: Condition Zero Deleted Scenes
+-- Created: September 29, 2025 at 05:21:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 23
+-- Total DLCs: 0
+-- Shared Depots: 15
+
+-- MAIN APPLICATION
+addappid(100) -- Counter-Strike: Condition Zero Deleted Scenes
+-- MAIN APP DEPOTS
+addappid(101, 1, "061d03331ce9449deb342be49d66503890adf5bd79e8fe8c27357cfecd3567cc") -- Condition Zero Deleted Scenes Base Content
+setManifestid(101, "8145292884440584734", 1261802851)
+addappid(104, 1, "70d203464b22b3827f057fb5b89568629c60f4afce46fc2e37cd34c9f159ee0e") -- Condition Zero Deleted Scenes French
+setManifestid(104, "308201953117119336", 116338795)
+addappid(105, 1, "10e422a74065a3cdb01a1c855db9a4f800df8bb6ed47a8187fa15bf8fbd0d644") -- Condition Zero Deleted Scenes Italian
+setManifestid(105, "7257628156378233827", 150002932)
+addappid(106, 1, "9e62afdc4eaa7a567fa6e46b9a4ec204e7f81c7ea5e5053a744d57314fefebf0") -- Condition Zero Deleted Scenes German
+setManifestid(106, "4475919498934197778", 151324130)
+addappid(107, 1, "3d7176acf7a7e8430500a581af779f7a4f00b8a63326abff1159ecbd0d781e7c") -- Condition Zero Deleted Scenes Spanish
+setManifestid(107, "1240446645381517062", 156441522)
+addappid(109, 1, "1d030a64d7f58061ce9ec962f0cc3ae5861a225005f4816aa4ad59224cf19c8f") -- Condition Zero Deleted Scenes sChinese
+setManifestid(109, "788390865382596531", 1790854)
+addappid(110, 1, "6a28b20061d6b0bd40c33dbedea6208c04245f74f2566b49ca0d95b47cfea496") -- Condition Zero Deleted Scenes Koreana
+setManifestid(110, "7767186978786576838", 1805110)
+addappid(111, 1, "6d29389b39a30511a01cfb2873fb775abc1bed1b1ebc5ccdcb4f72fa6a708ad3") -- Condition Zero Deleted Scenes tChinese
+setManifestid(111, "2283881628958687299", 1790648)
+-- SHARED DEPOTS (from other apps)
+addappid(11, 1, "dacfb281a0abbecc078e884aef2a69caf7723f4066ce3f493ea02248abc8de71") -- Counter-Strike Base Content (Shared from App 10)
+setManifestid(11, "3018093494972814332", 292340389)
+addappid(1, 1, "b465d45ab2a7c396f7d1c08a6644e68529ec86b14da77e18588abbbcd2412060") -- Base Goldsrc Shared Content (Shared from App 70)
+setManifestid(1, "2667117727132184734", 513399519)
+addappid(3, 1, "d249f7d93387a793ed021f0e2314ad0cea0d6aef0b0881c3b725a33deec37d59") -- Base Goldsrc Shared Platform (Shared from App 70)
+setManifestid(3, "6081070194444336449", 893958)
+addappid(9, 1, "68c92cf2532cdc6e0b7a9d2783b473b6d16bac72218badf68d7be9b033076096") -- Base Goldsrc OSX (Shared from App 70)
+setManifestid(9, "1083345737969856561", 55480049)
+addappid(8, 1, "20af1043db56d6408ddd965580e1269005b6e04f81c3676d155174f73099da95") -- Base Goldsrc Linux (Shared from App 70)
+setManifestid(8, "1360709870205537514", 127612567)
+addappid(2, 1, "fd681c065529f5ee95e920321a780a9290f8f31ef51c232ccb3e98d27e132ebc") -- Base Goldsrc Shared Binaries (Shared from App 70)
+setManifestid(2, "724247850320422500", 49814373)
+addappid(6, 1, "cd3d8b6f23fb863aea4d6789a53dd58dcf284141860e893e8e580d1988d93d3c") -- Base Goldsrc LV (Shared from App 70)
+setManifestid(6, "8518478957342391261", 52196711)
+addappid(72, 1, "bbc1a14b0c63025aece7d9c3b5a98089205580f321bb5edf6b5f00d368c6face") -- Half-Life French (Shared from App 70)
+setManifestid(72, "491735997354925434", 39331832)
+addappid(73, 1, "9604a5ba9ad4b580269d5362e201d217cdd9d268bf1098e2c6beb0c5ade020d3") -- Half-Life Italian (Shared from App 70)
+setManifestid(73, "3587203030189636184", 54692939)
+addappid(74, 1, "49d573f303205df2316749a6ede6725b74651fae3e845494b88ac3d0022a2857") -- Half-Life German (Shared from App 70)
+setManifestid(74, "6929797608257779911", 52412233)
+addappid(75, 1, "1339202607b6485322859fc1f045c6790bbc470a2d3bb8158aa94b06da31f9bb") -- Half-Life Spanish (Shared from App 70)
+setManifestid(75, "6599379689843151742", 48016168)
+addappid(77, 1, "793780217977cc3a005dc4410e963c980137e4faca4442f92dc41ab56af96983") -- Half-Life sChinese (Shared from App 70)
+setManifestid(77, "8307934900153578333", 45708546)
+addappid(78, 1, "39f04a0cc8cffae8023eff1e3e933680681997409823942a5f0f2899d2d2c4b2") -- Half-Life Koreana (Shared from App 70)
+setManifestid(78, "2582370374354695075", 45515236)
+addappid(79, 1, "019e1cb0950e060fb992001657a444fe03cbc4984081e0d1d9a6505259a7a7e7") -- Half-Life tChinese (Shared from App 70)
+setManifestid(79, "6488358189498321283", 45707416)
+addappid(141, 1, "0d85b74e163cceabf3651fdd6084585587461f6f7735c37eef875634fc6886cb") -- Half-Life Russian (Shared from App 70)
+setManifestid(141, "3921686991280371259", 45052)
