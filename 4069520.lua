376585a3e7abc244f3934ef627d8f2f8a3583037
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-22 14:29:13 +0700

    Tai moi Super Battle Golf via Bot

---
+++ 4069520.lua
@@ -0,0 +1,16 @@
+-- 4069520's Lua and Manifest Created by Hubcap Manifest
+-- Super Battle Golf
+-- Created: April 16, 2026 at 10:09:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4069520, 1, "0dfbf7a5cc6dfbbde7b7d1c6232822bfe89ff4a655427ec3bcd9e3e427c16776") -- Super Battle Golf
+-- MAIN APP DEPOTS
+addappid(4069521, 1, "7e03356de28c147831a4c06ff74efaa4af6dbeafdfffa371069c5635bd41cf00") -- Depot 4069521
+setManifestid(4069521, "1731867607556253764", 1818015642)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
