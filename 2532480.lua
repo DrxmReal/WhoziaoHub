18455efaf7267ff09f97cc0596070b98df39cb93
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-05-22 14:28:59 +0700

    Tai moi Warhammer 40,000: Mechanicus II via Bot

---
+++ 2532480.lua
@@ -0,0 +1,19 @@
+-- 2532480's Lua and Manifest Created by Hubcap Manifest
+-- Warhammer 40,000: Mechanicus II
+-- Created: May 21, 2026 at 13:04:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2532480) -- Warhammer 40,000: Mechanicus II
+-- MAIN APP DEPOTS
+addappid(2532481, 1, "2857e061c41a54f125a0113938f2b7a57a842e6db104d94f99fc6eba26eef7ad") -- Depot 2532481
+setManifestid(2532481, "3427348915842472948", 19810638188)
+-- DLCS WITH DEDICATED DEPOTS
+-- Warhammer 40,000 Mechanicus II  Digital Artbook (AppID: 4204170)
+addappid(4204170)
+addappid(4204170, 1, "02494aecd0c64174998b07f61d13e24dc083123b90315e05de6d6e588637365c") -- Warhammer 40,000 Mechanicus II  Digital Artbook - Depot 4204170
+setManifestid(4204170, "1903501890362474840", 525990626)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4328830) -- Warhammer 40,000 Mechanicus II  In-game Legacy Soundtrack
