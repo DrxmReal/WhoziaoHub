85cc81fff55a91588f4d09ee8ff30133b65f9ef5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-14 21:45:41 +0700

    Tai moi ABZU via Bot

---
+++ 384190.lua
@@ -0,0 +1,25 @@
+-- 384190's Lua and Manifest Created by Hubcap Manifest
+-- ABZÛ
+-- Created: September 28, 2025 at 21:24:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(384190) -- ABZÛ
+-- MAIN APP DEPOTS
+addappid(384191, 1, "5e3d2d48766f08b9e0feb762f7f850c7c50d54e19f83e8c55d98e61b05324696") -- ABZU Content
+setManifestid(384191, "1272401356118802888", 4818910704)
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- ABZU - Official Soundtrack (AppID: 577450)
+addappid(577450)
+addappid(577450, 1, "efbe34ac7ecfa79956ca5d53f43011169bc1f827522f7862d0350ff52fda0685") -- ABZU - Official Soundtrack - ABZU - Official Soundtrack (577450) Depot
+setManifestid(577450, "1879612128713196159", 148371748)
