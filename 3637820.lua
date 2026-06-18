90070b96c904859ef324c05993ea41df7292b08c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:09:56 +0700

    Tai moi Addy via Bot

---
+++ 3637820.lua
@@ -0,0 +1,12 @@
+-- 3637820's Lua and Manifest Created by Hubcap Manifest
+-- Addy
+-- Created: June 18, 2026 at 00:24:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3637820) -- Addy
+-- MAIN APP DEPOTS
+addappid(3637821, 1, "61957058ce0166f45e395f230632717c16d5394e88225a72282a8ac9084a050a") -- Depot 3637821
+setManifestid(3637821, "3329922213537074191", 510922312)
