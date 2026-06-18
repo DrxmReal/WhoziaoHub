6351dfb82dd82934a534a1b09c2332d78e595c59
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:14:48 +0700

    Tai moi Immortal Joan: Cyber Revolution via Bot

---
+++ 4400130.lua
@@ -0,0 +1,12 @@
+-- 4400130's Lua and Manifest Created by Hubcap Manifest
+-- Immortal Joan: Cyber Revolution
+-- Created: June 16, 2026 at 19:59:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4400130, 1, "77d3c5b363f2b57672b4d5b91a834d247043a27cfab8803cf7140679e2f43796") -- Immortal Joan: Cyber Revolution
+-- MAIN APP DEPOTS
+addappid(4400131, 1, "368c24fc63e4144afe8fb647e8caa9f69577738ad70d796bfcc126a7ae4a63b1") -- Depot 4400131
+setManifestid(4400131, "2350201583982851016", 1121475642)
