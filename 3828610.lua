5266380404ab86b997c1a0d3c0c2369afe2c66de
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:16:06 +0700

    Tai moi Beauties and the Stranded via Bot

---
+++ 3828610.lua
@@ -0,0 +1,12 @@
+-- 3828610's Lua and Manifest Created by Hubcap Manifest
+-- Beauties and the Stranded
+-- Created: June 16, 2026 at 04:20:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3828610) -- Beauties and the Stranded
+-- MAIN APP DEPOTS
+addappid(3828611, 1, "d83ace4e860aa2496b65c658d39a80cf2526035db2425125a971098586da15ae") -- Depot 3828611
+setManifestid(3828611, "4957304653003649581", 34882926238)
