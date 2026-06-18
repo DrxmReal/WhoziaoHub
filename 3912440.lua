1f7d48177e77f2cea92926a80eea5582b7898212
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:10:36 +0700

    Tai moi NULL.Process via Bot

---
+++ 3912440.lua
@@ -0,0 +1,12 @@
+-- 3912440's Lua and Manifest Created by Hubcap Manifest
+-- NULL.Process
+-- Created: June 18, 2026 at 00:23:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3912440) -- NULL.Process
+-- MAIN APP DEPOTS
+addappid(3912441, 1, "4e71629594227cc719cb5b24769ed9e4c302a449035524cbfd676cf56dcf05e3") -- Depot 3912441
+setManifestid(3912441, "3438977372496018185", 133996480)
