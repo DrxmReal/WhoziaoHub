1a48f780b873c94215e738e76c4f780e7555d795
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:10:29 +0700

    Tai moi Attack on Inirea via Bot

---
+++ 3633790.lua
@@ -0,0 +1,16 @@
+-- 3633790's Lua and Manifest Created by Hubcap Manifest
+-- Attack on Inirea
+-- Created: June 18, 2026 at 00:27:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3633790) -- Attack on Inirea
+-- MAIN APP DEPOTS
+addappid(3633791, 1, "33a88774e9996a32242ca068e1fa6a80a6ed7211dfdbfd89506aba142bdc58d7") -- Depot 3633791
+setManifestid(3633791, "6896804152290077341", 354379917)
+addappid(3633792, 1, "598669195d796c6f9ee77c238b13ae594970b34dfce9a876e95076e7795127bd") -- Depot 3633792
+setManifestid(3633792, "4593219988748412665", 378420219)
+addappid(3633793, 1, "72cf012201c3c792ed5b24e08d6c3eb202325444c300be84e480fcd368019cc4") -- Depot 3633793
+setManifestid(3633793, "1355923054014707265", 352383281)
