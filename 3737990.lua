d4b85bfe913acdc4ff7ce5ba85759ebf4f3c7c1d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:12:22 +0700

    Tai moi Isekat: Crushed by a Computer, My Beloved Kitten is Transported to a Fantasy World where its Typing Skills Save the Kingdom! via Bot

---
+++ 3737990.lua
@@ -0,0 +1,14 @@
+-- 3737990's Lua and Manifest Created by Hubcap Manifest
+-- Isekat
+-- Created: June 18, 2026 at 00:21:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3737990) -- Isekat
+-- MAIN APP DEPOTS
+addappid(3737991, 1, "fddd6b26b41be966153402fe6f51a4ff7f940debcce2e3028306e63d84bd0557") -- Depot 3737991
+setManifestid(3737991, "2629479374586579669", 4170176246)
+addappid(3737992, 1, "e138af684b827bb1027a0c76fd663341bee6f6036e67a172bcb50d32c9f8eee3") -- Depot 3737992
+setManifestid(3737992, "6389950036477962801", 4370623995)
