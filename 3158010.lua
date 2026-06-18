0ffa10e91ac23b263dd6a2d067c1d52363476305
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:12:29 +0700

    Tai moi Chaos in the harem via Bot

---
+++ 3158010.lua
@@ -0,0 +1,12 @@
+-- 3158010's Lua and Manifest Created by Hubcap Manifest
+-- 后宫大乱斗
+-- Created: June 18, 2026 at 00:19:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3158010) -- 后宫大乱斗
+-- MAIN APP DEPOTS
+addappid(3158011, 1, "972b31c590053e19e72fe4730af42fa22169fa7ee4964804bfa3afac5c9b1b18") -- Depot 3158011
+setManifestid(3158011, "796773242856359586", 890466638)
