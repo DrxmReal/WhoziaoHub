3b0b80a82b3465bd19d45d0666b373d3b15d787d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:10:46 +0700

    Tai moi Mate'Morphosis via Bot

---
+++ 3863030.lua
@@ -0,0 +1,16 @@
+-- 3863030's Lua and Manifest Created by Hubcap Manifest
+-- Mate'Morphosis
+-- Created: June 18, 2026 at 00:21:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3863030, 1, "63d6b13c5fd492b883ec6127c54cefa5d63e3dc393e991d01fdbcb353e62fc34") -- Mate'Morphosis
+-- MAIN APP DEPOTS
+addappid(3863034, 1, "6aefecf2feef7e005b8d8a7783c76662ade92cd72a688e1e1c00d4b71fadc222") -- Depot 3863034
+setManifestid(3863034, "6923457924583664469", 163385858)
+addappid(3863035, 1, "763efb21530d9aa2614a4c34a30a819c1c21f2ce9e419477df310ed5111dc426") -- Depot 3863035
+setManifestid(3863035, "7549002027518452054", 178041147)
+addappid(3863036, 1, "03f335797cfe526e2076bea507a0170679e7a3760e6002ac2ca18457398d0c8c") -- Depot 3863036
+setManifestid(3863036, "8483629453328039540", 160471677)
