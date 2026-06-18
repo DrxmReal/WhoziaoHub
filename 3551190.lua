d5ebfdb571040daac643119d63ffadec399e6d4b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 15:12:51 +0700

    Tai moi The Last Salvage Squad via Bot

---
+++ 3551190.lua
@@ -0,0 +1,12 @@
+-- 3551190's Lua and Manifest Created by Hubcap Manifest
+-- The Last Salvage Squad
+-- Created: June 17, 2026 at 15:12:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3551190, 1, "66d241d2988de914dda833691dc4a06c8be2d5cf8dec208bcc6a5dc206c57e55") -- The Last Salvage Squad
+-- MAIN APP DEPOTS
+addappid(3551191, 1, "55e819868459cda88c4a50b043e7bf42c886c75df477fe58ce3bc9bd8b7230d4") -- Depot 3551191
+setManifestid(3551191, "188159836968256168", 940663751)
