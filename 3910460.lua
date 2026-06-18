edad7b9001fbd73060177e2a60982f8c1ccd56f9
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-18 14:53:32 +0700

    Tai moi GraviChess via Bot

---
+++ 3910460.lua
@@ -0,0 +1,12 @@
+-- 3910460's Lua and Manifest Created by Hubcap Manifest
+-- GraviChess
+-- Created: June 18, 2026 at 00:23:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3910460) -- GraviChess
+-- MAIN APP DEPOTS
+addappid(3910461, 1, "fdd0a903a41203440b4676b3154133621a0bf7c71ae21890eecfba8952731287") -- Depot 3910461
+setManifestid(3910461, "40716001219127789", 27831564)
