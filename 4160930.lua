a8fe1935b00633f6f8329b94e59c9260fbd2f7c1
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:44:10 +0700

    Tai moi Destruction of Heavenly Bliss via Bot

---
+++ 4160930.lua
@@ -0,0 +1,14 @@
+-- 4160930's Lua and Manifest Created by Hubcap Manifest
+-- Destruction of Heavenly Bliss
+-- Created: June 20, 2026 at 02:29:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4160930) -- Destruction of Heavenly Bliss
+-- MAIN APP DEPOTS
+addappid(4160932, 1, "7a13bd9ceba64cf24fc826ef37ebdab31052d77afd756b5c3343c95e50bdbd80") -- Depot 4160932
+setManifestid(4160932, "1693428644165954726", 188982528)
+addappid(4160933, 1, "31dede3e96f44f47f7d06e8a5a7424f7e73d7e21e94c57a67e176cc5bd57e320") -- Depot 4160933
+setManifestid(4160933, "4330969824924487471", 215459304)
