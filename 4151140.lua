0b07b3e9805d536d0db1a9f25edf0a9c145a4dca
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:44:28 +0700

    Tai moi My Friend’s Girlfriend via Bot

---
+++ 4151140.lua
@@ -0,0 +1,12 @@
+-- 4151140's Lua and Manifest Created by Hubcap Manifest
+-- My Friend’s Girlfriend
+-- Created: June 20, 2026 at 01:21:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4151140) -- My Friend’s Girlfriend
+-- MAIN APP DEPOTS
+addappid(4151141, 1, "69bfe9bb4b5175484e7f715873d909c15dcf6ae05391bee51de756c0f26b6ab0") -- Depot 4151141
+setManifestid(4151141, "6357455114254721214", 2400324187)
