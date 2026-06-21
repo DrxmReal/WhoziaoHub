8d4e121501771a5a7ea4696298ccc773c810c0ec
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:43:28 +0700

    Tai moi Monologin via Bot

---
+++ 4692700.lua
@@ -0,0 +1,12 @@
+-- 4692700's Lua and Manifest Created by Hubcap Manifest
+-- Monologin
+-- Created: June 20, 2026 at 02:31:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4692700) -- Monologin
+-- MAIN APP DEPOTS
+addappid(4692701, 1, "6642616d93c5d00d5b2e483a3a6118bf1092877978ed91a02005d9b9b0f5675f") -- Depot 4692701
+setManifestid(4692701, "5664647597320729075", 935629532)
