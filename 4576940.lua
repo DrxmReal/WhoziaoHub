4720e6f2849579b6815c680a389a6dc4d12416c7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:42:48 +0700

    Tai moi Cozy Twinkie via Bot

---
+++ 4576940.lua
@@ -0,0 +1,12 @@
+-- 4576940's Lua and Manifest Created by Hubcap Manifest
+-- Cozy Twinkie
+-- Created: June 20, 2026 at 14:10:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4576940, 1, "494f0f71a6ed37fab04b9725d49d89e101b6cbe735177f43b1d858e42200a9e5") -- Cozy Twinkie
+-- MAIN APP DEPOTS
+addappid(4576941, 1, "310d4f700882b48f62f54270ba16fdd65ab1e41c67a20879ff86dadfed4b5fe8") -- Depot 4576941
+setManifestid(4576941, "4964306359905037752", 157235088)
