168a6fc38be08dbee199a06c9c6c280f3a2ae5a7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:43:10 +0700

    Tai moi Femboy Survival via Bot

---
+++ 3832120.lua
@@ -0,0 +1,12 @@
+-- 3832120's Lua and Manifest Created by Hubcap Manifest
+-- Femboy Survival
+-- Created: June 20, 2026 at 12:16:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3832120) -- Femboy Survival
+-- MAIN APP DEPOTS
+addappid(3832121, 1, "5fe0e6ae52258769eb74bc7a5f9438ffd660489495b7e15f347074e9f10bbc80") -- Depot 3832121
+setManifestid(3832121, "6677307511273697633", 3298703021)
