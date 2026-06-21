bb748177632b7084e5c91a34f813e7e8d77a51a0
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:46:51 +0700

    Tai moi With Your Coworker via Bot

---
+++ 4564320.lua
@@ -0,0 +1,14 @@
+-- 4564320's Lua and Manifest Created by Hubcap Manifest
+-- With Your Coworker
+-- Created: June 19, 2026 at 07:02:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4564320) -- With Your Coworker
+-- MAIN APP DEPOTS
+addappid(4564321, 1, "a920e3f45215788613a9c509e4822eb88aa0dbc1dd54a849dc657d44a08aeb2c") -- Depot 4564321
+setManifestid(4564321, "7506272848335857733", 320377080)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4564322) -- Depot 4564322 (empty depot)
