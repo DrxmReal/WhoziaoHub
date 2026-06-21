1411ce364004def4a7bcffa606827a1419ae1818
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:43:45 +0700

    Tai moi What You Didn't See via Bot

---
+++ 4555460.lua
@@ -0,0 +1,18 @@
+-- 4555460's Lua and Manifest Created by Hubcap Manifest
+-- What You Didn't See
+-- Created: June 20, 2026 at 02:29:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4555460) -- What You Didn't See
+-- MAIN APP DEPOTS
+addappid(4555461, 1, "49dde4464bba877028b4e6ca7f40b9da1051c8403037312d8ca91aa6afbd9c7d") -- Depot 4555461
+setManifestid(4555461, "5097685693855420237", 3539635844)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
