35a3a10a2f5f46ddde9e5b0af9054058552584bd
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:45:23 +0700

    Tai moi The Quiet Things via Bot

---
+++ 2288520.lua
@@ -0,0 +1,18 @@
+-- 2288520's Lua and Manifest Created by Hubcap Manifest
+-- The Quiet Things
+-- Created: June 19, 2026 at 12:38:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2288520, 1, "f59fbf058a1cee44e344ca678c1346f81d42df3ab5dd088d89ada2d89b98297c") -- The Quiet Things
+-- MAIN APP DEPOTS
+addappid(2288521, 1, "62e93e27c4860fa794fd951d61c47da4d3cb3a647db888522f1185b8b67f2476") -- Depot 2288521
+setManifestid(2288521, "9155687818722455403", 3047245622)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
