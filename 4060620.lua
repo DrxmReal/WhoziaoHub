7b0292ce3edadd3a8440ac0e949415d111980b28
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:43:01 +0700

    Tai moi Movavi Video Suite 2026 via Bot

---
+++ 4060620.lua
@@ -0,0 +1,69 @@
+-- 4060620's Lua and Manifest Created by Hubcap Manifest
+-- Movavi Video Suite 2026 Steam Edition
+-- Created: June 21, 2026 at 02:57:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 15
+-- Total DLCs: 14
+
+-- MAIN APPLICATION
+addappid(4060620, 1, "e1d9110e9ab046392d448beb4ef19329e764f437d54ce08ae694517015b2093d") -- Movavi Video Suite 2026 Steam Edition
+-- MAIN APP DEPOTS
+addappid(4060621, 1, "c76c0783400fec9cdb94972dcf65a6bf4f639d86448a0bf8d0ae244d66b6913d") -- Depot 4060621
+setManifestid(4060621, "4553406811315034732", 685299845)
+-- DLCS WITH DEDICATED DEPOTS
+-- Movavi Video Suite 2026 Steam Edition - Cinematic Set (AppID: 4090080)
+addappid(4090080)
+addappid(4090080, 1, "3a8c4c3926a646eeeffa0b35216c23f6905a1641db992c1daebf3090b8bbeb0e") -- Movavi Video Suite 2026 Steam Edition - Cinematic Set - Depot 4090080
+setManifestid(4090080, "3371478877398379720", 297062)
+-- Movavi Video Suite 2026 Steam Edition - My Channel Pack for YouTube 2 (AppID: 4090090)
+addappid(4090090)
+addappid(4090090, 1, "53cec8fcc0ccbd85d3b5c40f8d72a2d0e139b7c83d46eef026f9cb187c72e048") -- Movavi Video Suite 2026 Steam Edition - My Channel Pack for YouTube 2 - Depot 4090090
+setManifestid(4090090, "138017655385519212", 126847)
+-- Movavi Video Suite 2026 Steam Edition - Digital Overlay Pack (AppID: 4090100)
+addappid(4090100)
+addappid(4090100, 1, "e14fbb8dc680e11a9dbf3c56303c1c4261539fe0e51a221b9cf83a5a1b646a7a") -- Movavi Video Suite 2026 Steam Edition - Digital Overlay Pack - Depot 4090100
+setManifestid(4090100, "2976854948684646869", 18985)
+-- Movavi Video Suite 2026 Steam Edition - Text Projection Overlay Pack (AppID: 4090110)
+addappid(4090110)
+addappid(4090110, 1, "02c969f34de3c42d99f4377a0d86b83b9bb337d0d2ce948300a5c3527a721284") -- Movavi Video Suite 2026 Steam Edition - Text Projection Overlay Pack - Depot 4090110
+setManifestid(4090110, "1833258600944231397", 22414)
+-- Movavi Video Suite 2026 Steam Edition - Good Game Pack (AppID: 4090120)
+addappid(4090120)
+addappid(4090120, 1, "6d00ba90981e9f9beb8fd6615bd6747b2e0b2b5aeb677c25df28c4f937d5b540") -- Movavi Video Suite 2026 Steam Edition - Good Game Pack - Depot 4090120
+setManifestid(4090120, "317170965202192885", 68256)
+-- Movavi Video Suite 2026 Steam Edition - Vlog Titles Pack (AppID: 4090130)
+addappid(4090130)
+addappid(4090130, 1, "73ae534bebdbdb77fdd3fe94446cdfe60cca8b057f8b476acf209bc720c22a5e") -- Movavi Video Suite 2026 Steam Edition - Vlog Titles Pack - Depot 4090130
+setManifestid(4090130, "121690696545653535", 1262013)
+-- Movavi Video Suite 2026 Steam Edition - Dynamic Transitions Pack (AppID: 4090150)
+addappid(4090150)
+addappid(4090150, 1, "1d793578d2518c1b90e1b59e7a1050827c18ae23e1cd80fa3ef8735537b9210d") -- Movavi Video Suite 2026 Steam Edition - Dynamic Transitions Pack - Depot 4090150
+setManifestid(4090150, "1262081310682862883", 86684)
+-- Movavi Video Suite 2026 Steam Edition - Business Booster Pack (AppID: 4090160)
+addappid(4090160)
+addappid(4090160, 1, "4358cad342b4bc67a35414f6409781047115ae8b2c6b7e19b24c1fece3292439") -- Movavi Video Suite 2026 Steam Edition - Business Booster Pack - Depot 4090160
+setManifestid(4090160, "5325880116700140785", 1866289)
+-- Movavi Video Suite 2026 Steam Edition - Timer Pack (AppID: 4090170)
+addappid(4090170)
+addappid(4090170, 1, "dd3380076c2b15279336f062a16f6c8849d4989f0dbfade38a724705891480d1") -- Movavi Video Suite 2026 Steam Edition - Timer Pack - Depot 4090170
+setManifestid(4090170, "5726083231168743467", 51261)
+-- Movavi Video Suite 2026 Steam Edition - Emoji Speech Bubbles Pack (AppID: 4090180)
+addappid(4090180)
+addappid(4090180, 1, "fc5563b1b37de9876fb4b546bef9736c5c40b127eeadaa8b8d31b822689f98d0") -- Movavi Video Suite 2026 Steam Edition - Emoji Speech Bubbles Pack - Depot 4090180
+setManifestid(4090180, "7075178264379249758", 463767)
+-- Movavi Video Suite 2026 Steam Edition - Action Scene Pack (AppID: 4090190)
+addappid(4090190)
+addappid(4090190, 1, "fdcc0987ab3f31d6a1f1ca3ceba987277061f18a887fa17a3434f21c878da23d") -- Movavi Video Suite 2026 Steam Edition - Action Scene Pack - Depot 4090190
+setManifestid(4090190, "7124847658353426260", 549165)
+-- Movavi Video Suite 2026 Steam Edition - Click The Bell Pack (AppID: 4090200)
+addappid(4090200)
+addappid(4090200, 1, "fef066590f70b6654c0a5ff9230da382fdafabaf3781d2e9033749aa9fa72d89") -- Movavi Video Suite 2026 Steam Edition - Click The Bell Pack - Depot 4090200
+setManifestid(4090200, "7829375631019729948", 109902)
+-- Movavi Video Suite 2026 Steam Edition - Pop Science Pack (AppID: 4090210)
+addappid(4090210)
+addappid(4090210, 1, "a406c37dd0564a1ef8d082ddf6ab36f2c96ef36e11a8384028076a6caf555921") -- Movavi Video Suite 2026 Steam Edition - Pop Science Pack - Depot 4090210
+setManifestid(4090210, "8496960173031892378", 741990)
+-- Movavi Video Suite 2026 Steam Edition - Racing Pack (AppID: 4090220)
+addappid(4090220)
+addappid(4090220, 1, "bb8d89c23b441e7bf64218da7ab7b8051bf4a15716944f488ec81242ca67835f") -- Movavi Video Suite 2026 Steam Edition - Racing Pack - Depot 4090220
+setManifestid(4090220, "6942185638145544629", 69838)
