3e843945a6ee3a2d5ac447fb44142f0f3f6e9867
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:45:16 +0700

    Tai moi 二万分の一の雨粒達 - One in 20,000 raindrops via Bot

---
+++ 4800570.lua
@@ -0,0 +1,12 @@
+-- 4800570's Lua and Manifest Created by Hubcap Manifest
+-- 二万分の一の雨粒達 - One in 20,000 raindrops
+-- Created: June 19, 2026 at 05:11:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4800570) -- 二万分の一の雨粒達 - One in 20,000 raindrops
+-- MAIN APP DEPOTS
+addappid(4800571, 1, "1657f50c70f0a86dcc9d06033078f21dc8052deec5fe5c051d098b33f58833dc") -- Depot 4800571
+setManifestid(4800571, "5685573615398935777", 735583589)
