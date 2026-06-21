dddda12b470d7a00a4c39d860c0f1a2e94108ad6
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:48:04 +0700

    Tai moi PokerVersity: Challenge via Bot

---
+++ 4592140.lua
@@ -0,0 +1,12 @@
+-- 4592140's Lua and Manifest Created by Hubcap Manifest
+-- PokerVersity: Challenge
+-- Created: June 18, 2026 at 05:30:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4592140) -- PokerVersity: Challenge
+-- MAIN APP DEPOTS
+addappid(4592141, 1, "a70bf78b377a9f6b440befe3ff748a319be1b5eb112943ff33ba1ee1cf207239") -- Depot 4592141
+setManifestid(4592141, "5001453521624753127", 359937621)
