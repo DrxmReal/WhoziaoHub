65e89a9f79437422fe91744acb086cf1df32ee56
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:43:54 +0700

    Tai moi Taktari via Bot

---
+++ 4642310.lua
@@ -0,0 +1,14 @@
+-- 4642310's Lua and Manifest Created by Hubcap Manifest
+-- Taktari
+-- Created: June 20, 2026 at 02:29:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4642310) -- Taktari
+-- MAIN APP DEPOTS
+addappid(4642311, 1, "907a1b5115410333b51a2957a434e777038c868f352a6e0bfbcfefa11c6a21c7") -- Depot 4642311
+setManifestid(4642311, "840502384483245762", 1732011240)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4642313) -- Depot 4642313 (empty depot)
