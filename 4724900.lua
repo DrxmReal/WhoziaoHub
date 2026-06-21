2e411432ebb52bad42054c1793440ae5d2e5352a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-21 15:44:20 +0700

    Tai moi Infinite Bread via Bot

---
+++ 4724900.lua
@@ -0,0 +1,12 @@
+-- 4724900's Lua and Manifest Created by Hubcap Manifest
+-- 无限面包
+-- Created: June 21, 2026 at 04:44:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4724900) -- 无限面包
+-- MAIN APP DEPOTS
+addappid(4724901, 1, "59df40acccf8c1da9f164296ee07db2f002125c2cf791ea852ebb9baffdfe438") -- Depot 4724901
+setManifestid(4724901, "969213069515679948", 103041504)
