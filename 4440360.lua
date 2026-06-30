0da54ac34515d3dd7613ae433be0dc3d54c7a766
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:45:02 +0700

    Tai moi Professional Cats via Bot

---
+++ 4440360.lua
@@ -0,0 +1,12 @@
+-- 4440360's Lua and Manifest Created by Hubcap Manifest
+-- Professional Cats
+-- Created: June 29, 2026 at 13:19:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4440360) -- Professional Cats
+-- MAIN APP DEPOTS
+addappid(4440361, 1, "37c30b8218a236f6c1293a57441ab3e9939a21f50d0093bf92ccac9703bc4c5e") -- Depot 4440361
+setManifestid(4440361, "1941073363876189505", 818164544)
