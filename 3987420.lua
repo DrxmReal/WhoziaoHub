514de014c46c3cb7c8ebc03cc267384ebc93448a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:55 +0700

    Tai moi Buenos Aires Mirror Line via Bot

---
+++ 3987420.lua
@@ -0,0 +1,12 @@
+-- 3987420's Lua and Manifest Created by Hubcap Manifest
+-- Buenos Aires Mirror Line
+-- Created: June 29, 2026 at 05:59:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3987420) -- Buenos Aires Mirror Line
+-- MAIN APP DEPOTS
+addappid(3987421, 1, "6259f818eb252cc0489235005e4f6a100cc1ebeeafc782f5e3017e34fb8f4b1e") -- Depot 3987421
+setManifestid(3987421, "5306729933622707725", 394401568)
