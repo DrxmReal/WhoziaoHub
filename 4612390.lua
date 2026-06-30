08dc447e939e71f7a5f807da210dc8ce8703c690
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:45:50 +0700

    Tai moi Heroes of Retribution via Bot

---
+++ 4612390.lua
@@ -0,0 +1,18 @@
+-- 4612390's Lua and Manifest Created by Hubcap Manifest
+-- Heroes of Retribution
+-- Created: June 29, 2026 at 13:18:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4612390) -- Heroes of Retribution
+-- MAIN APP DEPOTS
+addappid(4612391, 1, "29a6f12b2b850f65657a3128cd799688c0634c5a8aa011dbc2d18dd241b87f10") -- Depot 4612391
+setManifestid(4612391, "1265382687220466562", 10182926506)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
