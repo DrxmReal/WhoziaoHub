161442868e9685d1b7e6129f5ef302292db2874c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:14:45 +0700

    Tai moi Supernatural Visual Novel "The Fourteenth Ξ (Guzai) Vol. 1 & 2" via Bot

---
+++ 3795190.lua
@@ -0,0 +1,12 @@
+-- 3795190's Lua and Manifest Created by Hubcap Manifest
+-- 伝奇系ビジュアルノベル『十四番目のΞ（グザイ） 壱・弐』
+-- Created: June 29, 2026 at 09:08:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3795190) -- 伝奇系ビジュアルノベル『十四番目のΞ（グザイ） 壱・弐』
+-- MAIN APP DEPOTS
+addappid(3795191, 1, "b6cf3a8dc45855f8dc381f5bfc20ed5c15ecccba94a0c0afc4ec7405c5abf150") -- Depot 3795191
+setManifestid(3795191, "6244775936800185044", 591921108)
