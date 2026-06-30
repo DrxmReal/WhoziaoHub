cb4ddf818849b0da05c433d8fe542894a4228321
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:14:42 +0700

    Tai moi Calmed by the Dαrk via Bot

---
+++ 4286990.lua
@@ -0,0 +1,12 @@
+-- 4286990's Lua and Manifest Created by Hubcap Manifest
+-- Calmed by the Dαrk
+-- Created: June 29, 2026 at 13:17:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4286990) -- Calmed by the Dαrk
+-- MAIN APP DEPOTS
+addappid(4286991, 1, "1b1c050013f4e407f17f4a339279ac04c66d5998793bc6e19f60d70e28f4b20c") -- Depot 4286991
+setManifestid(4286991, "8992489603076847102", 646347607)
