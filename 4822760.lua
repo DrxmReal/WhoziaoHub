6cb5803983a83f24f1e9645bf28f96a660c76bc5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:47:25 +0700

    Tai moi WinWinWin via Bot

---
+++ 4822760.lua
@@ -0,0 +1,12 @@
+-- 4822760's Lua and Manifest Created by Hubcap Manifest
+-- WinWinWin
+-- Created: June 29, 2026 at 13:18:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4822760) -- WinWinWin
+-- MAIN APP DEPOTS
+addappid(4822761, 1, "aceec17c1d66128e27cf54000facae239182bcc71e09d9561b234930a40bfa23") -- Depot 4822761
+setManifestid(4822761, "7658797342199070026", 236029452)
