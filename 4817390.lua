f07103bb9687b3dbf9cc1023cac6915493078cc6
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:14:56 +0700

    Tai moi DIGDLE via Bot

---
+++ 4817390.lua
@@ -0,0 +1,14 @@
+-- 4817390's Lua and Manifest Created by Hubcap Manifest
+-- DIGDLE
+-- Created: June 29, 2026 at 07:36:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4817390) -- DIGDLE
+-- MAIN APP DEPOTS
+addappid(4817391, 1, "7f032fec843d6cfcc173c402226270c13e887484d665f55867a1c01cd45ff884") -- Depot 4817391
+setManifestid(4817391, "765226890967721961", 456270399)
+addappid(4817392, 1, "28856c490f3fc1355bc1f22f0ff402225aa830b6512766473ba22c2c7d3d21d0") -- Depot 4817392
+setManifestid(4817392, "7085694535926723290", 477899651)
