9910d1701a950bf0fbe43035cacc5d9da027b81e
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:11 +0700

    Tai moi Road To Glory via Bot

---
+++ 4540230.lua
@@ -0,0 +1,14 @@
+-- 4540230's Lua and Manifest Created by Hubcap Manifest
+-- Road To Glory
+-- Created: June 29, 2026 at 07:36:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4540230) -- Road To Glory
+-- MAIN APP DEPOTS
+addappid(4540231, 1, "ff3d36131da62bdd79e279120decd78e6668af606fee96d8683ed7ce00041903") -- Depot 4540231
+setManifestid(4540231, "6254401281936339641", 1466953711)
+addappid(4540232, 1, "d2c40c9443ee0342e3c8fccbc7dadedfc8f824ed4698a5c6ebda8b36f6b68f4b") -- Depot 4540232
+setManifestid(4540232, "6837177091029410558", 794453241)
