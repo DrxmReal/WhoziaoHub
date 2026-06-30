5e4af0b4f3004bf6cba9506b383b054080237b23
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:05 +0700

    Tai moi Coastal Summit Sports: College Athletics 27 via Bot

---
+++ 4692340.lua
@@ -0,0 +1,14 @@
+-- 4692340's Lua and Manifest Created by Hubcap Manifest
+-- Coastal Summit Sports: College Athletics 27
+-- Created: June 30, 2026 at 02:58:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4692340) -- Coastal Summit Sports: College Athletics 27
+-- MAIN APP DEPOTS
+addappid(4692341, 1, "3edfa7643d55c70c1d171c5af9f79eeb10a16611141a64bfd35be21e88a76d8f") -- Depot 4692341
+setManifestid(4692341, "3821714566436494134", 1520547471)
+addappid(4692342, 1, "1ee1f7bf8948a974261f63f0573458b6a26d13680c6486b4512f1037b83089b6") -- Depot 4692342
+setManifestid(4692342, "8898407876616927725", 603940161)
