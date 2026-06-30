75389dbacb2d098ae0365273baa95e348db8386f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:19 +0700

    Tai moi Bomb Bowling X via Bot

---
+++ 3552920.lua
@@ -0,0 +1,12 @@
+-- 3552920's Lua and Manifest Created by Hubcap Manifest
+-- Bomb Bowling X
+-- Created: June 29, 2026 at 20:04:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3552920) -- Bomb Bowling X
+-- MAIN APP DEPOTS
+addappid(3552921, 1, "e69c4b699ffcc46114b790ea86302d90894f80262c6dd1e556f3660b2b70567d") -- Depot 3552921
+setManifestid(3552921, "8480115630291401104", 133597230)
