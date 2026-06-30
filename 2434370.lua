d6a738386b06e971d2b094a26aed87fab98a5e73
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:49:44 +0700

    Tai moi Auto Dungeon:Idle Adventure via Bot

---
+++ 2434370.lua
@@ -0,0 +1,12 @@
+-- 2434370's Lua and Manifest Created by Hubcap Manifest
+-- 放置地牢
+-- Created: June 30, 2026 at 02:57:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2434370) -- 放置地牢
+-- MAIN APP DEPOTS
+addappid(2434371, 1, "8b8e45d19d3fa99b66d12a1275c434f33e83f2c328b6b8b3173689af13cf03eb") -- Depot 2434371
+setManifestid(2434371, "6210086684221166725", 1161682398)
