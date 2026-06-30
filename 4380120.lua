65ea0c15f1445dc09e6d64dedbd593c03d2a75de
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:46:36 +0700

    Tai moi Horn of the Warlord via Bot

---
+++ 4380120.lua
@@ -0,0 +1,16 @@
+-- 4380120's Lua and Manifest Created by Hubcap Manifest
+-- Horn of the Warlord
+-- Created: June 30, 2026 at 02:56:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4380120) -- Horn of the Warlord
+-- MAIN APP DEPOTS
+addappid(4380121, 1, "89a8bdd463adf79f57537af78a058448cc52e4c8e60200765e0002a2ad489224") -- Depot 4380121
+setManifestid(4380121, "1320299432389600298", 834347744)
+addappid(4380122, 1, "12fd510f2d4dc3177bf67106d4d777f7b4f54bd20debae25aa8a3d7be3aad99a") -- Depot 4380122
+setManifestid(4380122, "2254445602927259587", 975651328)
+addappid(4380123, 1, "ee3b25874354365b5feec8952294116fbcada2ebd1b64dbe4071d6e5d1ebc413") -- Depot 4380123
+setManifestid(4380123, "8139347707057081004", 1044994867)
