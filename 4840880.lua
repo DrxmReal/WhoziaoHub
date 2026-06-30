0c7263a59cae25ce691a3d8c3a7492d38896acbc
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:16:03 +0700

    Tai moi ESKINITA via Bot

---
+++ 4840880.lua
@@ -0,0 +1,12 @@
+-- 4840880's Lua and Manifest Created by Hubcap Manifest
+-- ESKINITA
+-- Created: June 29, 2026 at 04:59:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4840880) -- ESKINITA
+-- MAIN APP DEPOTS
+addappid(4840881, 1, "f3832461c34bd1bbd7bb1f18073ae6042e4cb0390f11307d7c5a86201b5959c3") -- Depot 4840881
+setManifestid(4840881, "349358608180606498", 517624610)
