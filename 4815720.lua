8b679fc29911cf44080b5a8f9744fe404cffab77
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:46:49 +0700

    Tai moi Portalborn via Bot

---
+++ 4815720.lua
@@ -0,0 +1,12 @@
+-- 4815720's Lua and Manifest Created by Hubcap Manifest
+-- Portalborn
+-- Created: June 30, 2026 at 01:12:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4815720) -- Portalborn
+-- MAIN APP DEPOTS
+addappid(4815721, 1, "82b8ce514dc89b8cc5a9910e901014a032d53da97d921197be95022fb4df26e7") -- Depot 4815721
+setManifestid(4815721, "150785365698210947", 598785369)
