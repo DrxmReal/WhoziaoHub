3400e2185df468b4cea341f91c0cd163dad443ff
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:45:22 +0700

    Tai moi Demon Princess Retreat via Bot

---
+++ 4593710.lua
@@ -0,0 +1,12 @@
+-- 4593710's Lua and Manifest Created by Hubcap Manifest
+-- Demon Princess Retreat
+-- Created: June 29, 2026 at 13:18:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4593710) -- Demon Princess Retreat
+-- MAIN APP DEPOTS
+addappid(4593711, 1, "9fd94607b2deee89a5b25c51c6dfdd5dc56660def3efe3558540bdd4fe1ad100") -- Depot 4593711
+setManifestid(4593711, "4084119764047472090", 665236421)
