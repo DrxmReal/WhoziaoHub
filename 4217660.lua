28eea0a23805aa46c1578713f1fb0fd2d244cec0
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:47 +0700

    Tai moi Small Hero via Bot

---
+++ 4217660.lua
@@ -0,0 +1,12 @@
+-- 4217660's Lua and Manifest Created by Hubcap Manifest
+-- Small Hero
+-- Created: June 29, 2026 at 06:48:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4217660) -- Small Hero
+-- MAIN APP DEPOTS
+addappid(4217661, 1, "42cc19b6e183cc69c7e8391f2997026f9deda1e08ced2e637cd2a29dd75be2b9") -- Depot 4217661
+setManifestid(4217661, "136986871442139916", 214796003)
