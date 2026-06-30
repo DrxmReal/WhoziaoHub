46424d90c1d1ac034b87b90236d44aaee516d126
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:14:59 +0700

    Tai moi Mental Medicine via Bot

---
+++ 4785600.lua
@@ -0,0 +1,12 @@
+-- 4785600's Lua and Manifest Created by Hubcap Manifest
+-- Mental Medicine
+-- Created: June 29, 2026 at 07:36:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4785600) -- Mental Medicine
+-- MAIN APP DEPOTS
+addappid(4785601, 1, "dadc1b4c8a64e75d3d3370d20ef574bc1cbdf0d35b752b173b30590d4c763955") -- Depot 4785601
+setManifestid(4785601, "5991937290338095195", 279084324)
