cfc81bdc60148d0afe64f72e539f7a2de705466e
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:37 +0700

    Tai moi Quacky via Bot

---
+++ 4554770.lua
@@ -0,0 +1,14 @@
+-- 4554770's Lua and Manifest Created by Hubcap Manifest
+-- Quacky
+-- Created: June 29, 2026 at 16:48:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4554770) -- Quacky
+-- MAIN APP DEPOTS
+addappid(4554771, 1, "642f7cae3b857f1426af95084f8dc25d014bda7d2888bcaff6caccebaade2913") -- Depot 4554771
+setManifestid(4554771, "395174105169553899", 258774629)
+addappid(4554772, 1, "6564b4acd03c7a3b86fa150dcfb3bc20d8a8e8bd7b4216421a69bded26f506c1") -- Depot 4554772
+setManifestid(4554772, "4120778708527952671", 267512080)
