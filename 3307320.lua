8e470226af435f6ca3bcc89049b77d0f231569d2
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:47:36 +0700

    Tai moi Fool's Guild via Bot

---
+++ 3307320.lua
@@ -0,0 +1,18 @@
+-- 3307320's Lua and Manifest Created by Hubcap Manifest
+-- Fool's Guild
+-- Created: June 29, 2026 at 13:17:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3307320) -- Fool's Guild
+-- MAIN APP DEPOTS
+addappid(3307323, 1, "a134b82e15b2c88f00bc66f5be2226bd252a505ccff6cae33023a7fc35d35d1e") -- Depot 3307323
+setManifestid(3307323, "5641521946605898660", 141307256)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3307321) -- Depot 3307321 (empty depot)
