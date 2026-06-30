08c5a8b8e8d5f989022f494bc46f351cb618c58a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:15 +0700

    Tai moi idle:Doodle Realm via Bot

---
+++ 4270610.lua
@@ -0,0 +1,12 @@
+-- 4270610's Lua and Manifest Created by Hubcap Manifest
+-- 放置:纸色新境
+-- Created: June 30, 2026 at 02:59:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4270610) -- 放置:纸色新境
+-- MAIN APP DEPOTS
+addappid(4270611, 1, "857d50525b251d140ce8ae0d20c24778d930b1370aeae007d661bcdc0d769992") -- Depot 4270611
+setManifestid(4270611, "633318712816343403", 545729288)
