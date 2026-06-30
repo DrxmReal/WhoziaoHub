909d0916f357c6ffcf4109050f9fcb3a4ef5f12a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:16:08 +0700

    Tai moi Onsen 1313 via Bot

---
+++ 3252280.lua
@@ -0,0 +1,18 @@
+-- 3252280's Lua and Manifest Created by Hubcap Manifest
+-- Onsen 1313
+-- Created: June 29, 2026 at 04:59:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3252280) -- Onsen 1313
+-- MAIN APP DEPOTS
+addappid(3252281, 1, "380d5840ec1d0197b5caef62440eb858d048fe827238c4629d7e6f1d859feda0") -- Depot 3252281
+setManifestid(3252281, "4825756870200757743", 501148017)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
