ea4b3e2211ead13504f2c3d2929bf7bd3f148bb7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:14:50 +0700

    Tai moi Our Burial Dolls Remastered via Bot

---
+++ 4108290.lua
@@ -0,0 +1,12 @@
+-- 4108290's Lua and Manifest Created by Hubcap Manifest
+-- Our Burial Dolls Remastered
+-- Created: June 29, 2026 at 09:06:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4108290) -- Our Burial Dolls Remastered
+-- MAIN APP DEPOTS
+addappid(4108291, 1, "5d9284af4170c5906d350c3073b2c47496a215384f833cc25d8e5d0d9b284ee9") -- Depot 4108291
+setManifestid(4108291, "1197238166820214949", 147166574)
