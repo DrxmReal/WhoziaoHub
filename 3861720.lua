f795a01a5e39143b3202e334344114df7d97f6a8
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:00 +0700

    Tai moi Ruby's Realms via Bot

---
+++ 3861720.lua
@@ -0,0 +1,12 @@
+-- 3861720's Lua and Manifest Created by Hubcap Manifest
+-- Ruby's Realms
+-- Created: June 30, 2026 at 03:13:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3861720) -- Ruby's Realms
+-- MAIN APP DEPOTS
+addappid(3861721, 1, "73f4c25f0fb1176c0d03806f77099bfd4c829de3dbc6cda2ef584a46ea5a0c51") -- Depot 3861721
+setManifestid(3861721, "910365833218212844", 275210483)
