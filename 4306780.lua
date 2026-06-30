2ccbca3e90ce3b4cb6d2a657c99aab92bd3058e3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:45:56 +0700

    Tai moi Integrity via Bot

---
+++ 4306780.lua
@@ -0,0 +1,12 @@
+-- 4306780's Lua and Manifest Created by Hubcap Manifest
+-- Integrity
+-- Created: June 29, 2026 at 13:18:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4306780) -- Integrity
+-- MAIN APP DEPOTS
+addappid(4306781, 1, "8a44349773a917ce04064694002074fc1a101cd3cce8aede6a3a25f4ff199126") -- Depot 4306781
+setManifestid(4306781, "1912594139491394015", 1720320865)
