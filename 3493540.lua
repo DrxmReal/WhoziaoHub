2da979cc962924f513eaafab58fed6a753b70650
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:58 +0700

    Tai moi Transport Fever 3 via Bot

---
+++ 3493540.lua
@@ -0,0 +1,19 @@
+-- 3493540's Lua and Manifest Created by Hubcap Manifest
+-- Transport Fever 3
+-- Created: June 29, 2026 at 05:00:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3493540) -- Transport Fever 3
+addtoken(3493540, "12729446192953376118")
+-- MAIN APP DEPOTS
+addappid(3493542, 1, "2ccd7e92c47a0da5141377e6adb2a243206b24e3de8272b887854b93ddb77128") -- Depot 3493542
+setManifestid(3493542, "396487254870897184", 0)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3493541) -- Depot 3493541 (empty depot)
+-- addappid(3493543) -- Depot 3493543 (empty depot)
+-- addappid(3493544) -- Depot 3493544 (empty depot)
+-- addappid(3979620) -- Depot 3979620 (empty depot)
+-- addappid(3979630) -- Depot 3979630 (empty depot)
