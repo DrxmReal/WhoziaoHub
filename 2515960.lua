7ade7b6397ff96a45f3817f5bb2f93669e91eff7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:43 +0700

    Tai moi Validus mortis via Bot

---
+++ 2515960.lua
@@ -0,0 +1,12 @@
+-- 2515960's Lua and Manifest Created by Hubcap Manifest
+-- Validus mortis
+-- Created: June 29, 2026 at 07:35:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2515960) -- Validus mortis
+-- MAIN APP DEPOTS
+addappid(2515961, 1, "0ca414be09e052ad447ecd398e8eee9d3d79effde37ea2ef7b56c466003bed96") -- Depot 2515961
+setManifestid(2515961, "5878666103568221712", 25020942)
