f14ce015f0ddfeb88e78fa017a9a16fe1c19e186
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:04 +0700

    Tai moi The Wedding via Bot

---
+++ 2777430.lua
@@ -0,0 +1,12 @@
+-- 2777430's Lua and Manifest Created by Hubcap Manifest
+-- The Wedding
+-- Created: June 30, 2026 at 00:33:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2777430) -- The Wedding
+-- MAIN APP DEPOTS
+addappid(2777431, 1, "7ccbdac26df728169b47052c83f8d61edbb306bb2b764e13a00d3c1adbc60f19") -- Depot 2777431
+setManifestid(2777431, "8186092641875026592", 578292049)
