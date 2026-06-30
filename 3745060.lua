5037e871b718e3ae1a5e01a57e5f495da2ca9969
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:45:33 +0700

    Tai moi Dracs After Dusk via Bot

---
+++ 3745060.lua
@@ -0,0 +1,12 @@
+-- 3745060's Lua and Manifest Created by Hubcap Manifest
+-- Dracs After Dusk
+-- Created: June 29, 2026 at 13:18:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3745060) -- Dracs After Dusk
+-- MAIN APP DEPOTS
+addappid(3745061, 1, "0b902de65feadd93702cc51a5a4c37d65d7008a50c255c6b7411749e1101b744") -- Depot 3745061
+setManifestid(3745061, "2304185600355737172", 167367296)
