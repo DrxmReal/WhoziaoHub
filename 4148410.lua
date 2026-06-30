3e1776a2167b2253a1d87c95b99be5d441a7dd3d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:27 +0700

    Tai moi Cubic Spree via Bot

---
+++ 4148410.lua
@@ -0,0 +1,12 @@
+-- 4148410's Lua and Manifest Created by Hubcap Manifest
+-- Cubic Spree
+-- Created: June 29, 2026 at 07:35:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4148410) -- Cubic Spree
+-- MAIN APP DEPOTS
+addappid(4148411, 1, "07a6ddbc23d073d44b5dedf1395e24bad29018993f8322de2ec3d1a02fe475d0") -- Depot 4148411
+setManifestid(4148411, "7342745921138733865", 369133387)
