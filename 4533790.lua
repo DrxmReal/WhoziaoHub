01ecedbb456212486c8ee9e20c3dc75f8a11383d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:46:04 +0700

    Tai moi Escape the Red Curse via Bot

---
+++ 4533790.lua
@@ -0,0 +1,12 @@
+-- 4533790's Lua and Manifest Created by Hubcap Manifest
+-- Escape the Red Curse
+-- Created: June 29, 2026 at 13:18:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4533790) -- Escape the Red Curse
+-- MAIN APP DEPOTS
+addappid(4533791, 1, "813589afc127a31f92f401e66bb5ab1e0d5c85f48f617f027e5918b6177c0eb1") -- Depot 4533791
+setManifestid(4533791, "1893791557267388269", 165012216)
