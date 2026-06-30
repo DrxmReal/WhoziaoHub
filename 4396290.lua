55ac5f44f64e3f11097edf7249d5dae7227a5148
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:16:12 +0700

    Tai moi Cloudsea Frontier via Bot

---
+++ 4396290.lua
@@ -0,0 +1,12 @@
+-- 4396290's Lua and Manifest Created by Hubcap Manifest
+-- Cloudsea Frontier
+-- Created: June 29, 2026 at 04:59:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4396290) -- Cloudsea Frontier
+-- MAIN APP DEPOTS
+addappid(4396291, 1, "bba11e516e88315a382aabb926c17cbc1a243cf074e001aa7a1f45a13d8e99bd") -- Depot 4396291
+setManifestid(4396291, "946351944644931384", 826196522)
