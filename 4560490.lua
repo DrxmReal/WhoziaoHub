6627239a4fca0682b69471fc4a090339c005cb83
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:25 +0700

    Tai moi REWIND via Bot

---
+++ 4560490.lua
@@ -0,0 +1,16 @@
+-- 4560490's Lua and Manifest Created by Hubcap Manifest
+-- REWIND
+-- Created: June 29, 2026 at 17:37:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4560490) -- REWIND
+-- MAIN APP DEPOTS
+addappid(4560491, 1, "e4903202eceb6c355bb7fc461345c111d888809b420a923ff87c28f9f77a82fa") -- Depot 4560491
+setManifestid(4560491, "3360075043710333021", 1424816623)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
