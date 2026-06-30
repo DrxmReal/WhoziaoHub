891c1b863a2aa26c216f10d5ed5f8c2321c4919d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:16:17 +0700

    Tai moi The Office Void via Bot

---
+++ 4555950.lua
@@ -0,0 +1,20 @@
+-- 4555950's Lua and Manifest Created by Hubcap Manifest
+-- The Office Void
+-- Created: June 29, 2026 at 04:59:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4555950) -- The Office Void
+-- MAIN APP DEPOTS
+addappid(4555951, 1, "f3ecc4e5bc58b6d15e592d5420fd5b32cf269e0dd7cc87f9712670b46bb1a5de") -- Depot 4555951
+setManifestid(4555951, "6029029590959900418", 1049126254)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (Shared from App 228980)
+setManifestid(229020, "5799761707845834510", 810085)
