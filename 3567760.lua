df81b7cfe045b97b505b8be1138efa6b8c111468
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:35 +0700

    Tai moi Twig's Adventure 64 via Bot

---
+++ 3567760.lua
@@ -0,0 +1,14 @@
+-- 3567760's Lua and Manifest Created by Hubcap Manifest
+-- Twig's Adventure 64
+-- Created: June 29, 2026 at 07:35:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3567760, 1, "fa155b8115e639560b7610149c5ae7d39e87cf48a67af7b2ec3324a4994d9149") -- Twig's Adventure 64
+-- MAIN APP DEPOTS
+addappid(3567761, 1, "3b70e973d9a022c5ff685d8b3fc6cbbd36a06d0e849cf63a349a12b0239ae5db") -- Depot 3567761
+setManifestid(3567761, "6405606760226205600", 325667376)
+addappid(3567762, 1, "cf502fd351b2974b65be200f37f49ad642dffbfeaa9947f63b35f1587760d836") -- Depot 3567762
+setManifestid(3567762, "2160433258095674452", 298431920)
