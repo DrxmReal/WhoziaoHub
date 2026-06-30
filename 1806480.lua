7a4a6a6f1b3b2aba64eb74affa1d11038c074363
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:43 +0700

    Tai moi RIDLE via Bot

---
+++ 1806480.lua
@@ -0,0 +1,12 @@
+-- 1806480's Lua and Manifest Created by Hubcap Manifest
+-- RIDLE
+-- Created: June 29, 2026 at 13:42:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1806480) -- RIDLE
+-- MAIN APP DEPOTS
+addappid(1806481, 1, "46fb91bff7e29dfec4b04310578c09e4160f152ceccca933df525226d4fa3c87") -- Depot 1806481
+setManifestid(1806481, "7002188832472412703", 1368003912)
