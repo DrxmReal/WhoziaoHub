52628bf6a89764e7a9c2bdec21af1f56a42f621b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:47 +0700

    Tai moi A Robot's Tiny Adventure via Bot

---
+++ 3207280.lua
@@ -0,0 +1,14 @@
+-- 3207280's Lua and Manifest Created by Hubcap Manifest
+-- A Robot's Tiny Adventure
+-- Created: June 29, 2026 at 07:35:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3207280, 1, "9da488aa196d0559e4cf53ce10ccbc49fadb4d23c0e0d68dd26497a9c05d73d3") -- A Robot's Tiny Adventure
+-- MAIN APP DEPOTS
+addappid(3207281, 1, "2521ec6f8beb401af423a2ebe286d934cfc15e8e03df4eb7535344fdb68b3280") -- Depot 3207281
+setManifestid(3207281, "144764192214870999", 212124864)
+addappid(3207282, 1, "bc30626e8848cbb502b194789a42ccdcdd2654a7067734394e4beadaa9753915") -- Depot 3207282
+setManifestid(3207282, "5554832425993802251", 184292760)
