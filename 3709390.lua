f2dedf0043b5bf6370bba0d1fc2733cf851fc4d9
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 16:15:31 +0700

    Tai moi Gold and Bricks via Bot

---
+++ 3709390.lua
@@ -0,0 +1,14 @@
+-- 3709390's Lua and Manifest Created by Hubcap Manifest
+-- Gold and Bricks
+-- Created: June 29, 2026 at 07:35:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3709390, 1, "d5e23e25f0dad8a919c3fc7c8a88858e44b91b5ab52fec17c098f33cb1132b24") -- Gold and Bricks
+-- MAIN APP DEPOTS
+addappid(3709391, 1, "52c27ed4e85c670d988b70f783ff2fbd95dd045acd738ad11390c07cdb9820ba") -- Depot 3709391
+setManifestid(3709391, "4002414324283992225", 128558016)
+addappid(3709392, 1, "24d88f3bf17338edbc32a7074f3af0e6d5659576e4f502a39bd7835d66a48c96") -- Depot 3709392
+setManifestid(3709392, "1465067062527026152", 101322560)
