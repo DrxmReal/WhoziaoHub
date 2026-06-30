5e9c36ec74984b434fa84d5259fc5aea71ee8346
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:32 +0700

    Tai moi The Wackytown Game via Bot

---
+++ 4775340.lua
@@ -0,0 +1,12 @@
+-- 4775340's Lua and Manifest Created by Hubcap Manifest
+-- The Wackytown Game
+-- Created: June 29, 2026 at 16:55:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4775340) -- The Wackytown Game
+-- MAIN APP DEPOTS
+addappid(4775341, 1, "073996dcd6b7a061475ccd4049fbd056911f1a32585b6b99ce5ec54ad75e411f") -- Depot 4775341
+setManifestid(4775341, "6389383250149251790", 149770691)
