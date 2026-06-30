e822b09047afe331c5f97e5523116a6824817be9
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:46:42 +0700

    Tai moi 逐末之地：60s Exploring the Endless Land：60s via Bot

---
+++ 4380690.lua
@@ -0,0 +1,12 @@
+-- 4380690's Lua and Manifest Created by Hubcap Manifest
+-- 逐末之地：60s Exploring the Endless Land：60s
+-- Created: June 30, 2026 at 02:57:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4380690) -- 逐末之地：60s Exploring the Endless Land：60s
+-- MAIN APP DEPOTS
+addappid(4380691, 1, "422b14abf30fde8954d3bf2087c05eb7b75cf55029cbfc2bf8468451d693cf7c") -- Depot 4380691
+setManifestid(4380691, "1908867320245480897", 8803796720)
