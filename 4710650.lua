7da1f8e688e0a6d06807c420974fa4d227cf7670
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-06-30 15:44:14 +0700

    Tai moi The Shu legend via Bot

---
+++ 4710650.lua
@@ -0,0 +1,12 @@
+-- 4710650's Lua and Manifest Created by Hubcap Manifest
+-- 白书传说
+-- Created: June 29, 2026 at 21:06:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4710650) -- 白书传说
+-- MAIN APP DEPOTS
+addappid(4710651, 1, "56df9e7f2e55c8d9baaf0c0591940bfd1aa6f7595da9a97d2a4281d2ca853b57") -- Depot 4710651
+setManifestid(4710651, "7809084494390988667", 1387704605)
