4f23ade5ad63bd37e2f9ececd4f9b7b94b691411
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:33:14 +0700

    Tai moi Open Fishing XL via Bot

---
+++ 2230540.lua
@@ -0,0 +1,12 @@
+-- 2230540's Lua and Manifest Created by Hubcap Manifest
+-- Open Fishing XL
+-- Created: July 01, 2026 at 07:32:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2230540) -- Open Fishing XL
+-- MAIN APP DEPOTS
+addappid(2230541, 1, "268feb0cad1824ca2ee004e323a8a4e76ca48d0ff6de5215528e06003967c341") -- Depot 2230541
+setManifestid(2230541, "7155316854815880358", 265317083)
