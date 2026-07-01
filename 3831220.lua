4dcfd99814045bd3e36c0128dafa62eea47492d7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:32:40 +0700

    Tai moi Is this a Game yet? via Bot

---
+++ 3831220.lua
@@ -0,0 +1,12 @@
+-- 3831220's Lua and Manifest Created by Hubcap Manifest
+-- Is this a Game yet?
+-- Created: July 01, 2026 at 07:33:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3831220) -- Is this a Game yet?
+-- MAIN APP DEPOTS
+addappid(3831221, 1, "a17e9f9280b42a916faf85dd1a1ebd0dd6c31475f0bf21a315036d45812cb46c") -- Depot 3831221
+setManifestid(3831221, "1233167353867369895", 223552744)
