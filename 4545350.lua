0187b6b71bf11a01ee57de9c518593e07a6748ed
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:32:46 +0700

    Tai moi The Pissing Game via Bot

---
+++ 4545350.lua
@@ -0,0 +1,12 @@
+-- 4545350's Lua and Manifest Created by Hubcap Manifest
+-- The Pissing Game
+-- Created: July 01, 2026 at 07:33:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4545350) -- The Pissing Game
+-- MAIN APP DEPOTS
+addappid(4545351, 1, "ccf477146c0b7bd516a80c3347b8bc2c384b95c4861b31fe1546d1dc3d8e691b") -- Depot 4545351
+setManifestid(4545351, "3539332748349564955", 352529407)
