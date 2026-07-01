33f2a9d6985a9479a2ee3380be5d163a87d09c09
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:33:09 +0700

    Tai moi Haunted ROM: The Lost Cartridge via Bot

---
+++ 3897990.lua
@@ -0,0 +1,12 @@
+-- 3897990's Lua and Manifest Created by Hubcap Manifest
+-- Haunted ROM: The Lost Cartridge
+-- Created: July 01, 2026 at 07:33:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3897990) -- Haunted ROM: The Lost Cartridge
+-- MAIN APP DEPOTS
+addappid(3897991, 1, "e0ef42dd656601e68a83ed57420ada315d50005c1009bad46898fcadc51279ea") -- Depot 3897991
+setManifestid(3897991, "244499455602041538", 4584513262)
