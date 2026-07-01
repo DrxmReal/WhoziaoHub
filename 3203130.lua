c59c533e49e846dc1669af87e1a1f27e1012c278
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:33:26 +0700

    Tai moi Afterlight Catacombs via Bot

---
+++ 3203130.lua
@@ -0,0 +1,12 @@
+-- 3203130's Lua and Manifest Created by Hubcap Manifest
+-- Afterlight Catacombs
+-- Created: July 01, 2026 at 07:31:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3203130) -- Afterlight Catacombs
+-- MAIN APP DEPOTS
+addappid(3203131, 1, "5ddcfb0de6c3e988daa040060988efe6093429546fc1f9a4d59566e575dc2e8a") -- Depot 3203131
+setManifestid(3203131, "2140794172789843739", 450843945)
