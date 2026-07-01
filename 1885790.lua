142d147fbf362ee2c441e1ba0defef2ef17c5328
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:33:33 +0700

    Tai moi Untravelled Planet via Bot

---
+++ 1885790.lua
@@ -0,0 +1,18 @@
+-- 1885790's Lua and Manifest Created by Hubcap Manifest
+-- Untravelled Planet
+-- Created: July 01, 2026 at 07:29:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1885790) -- Untravelled Planet
+-- MAIN APP DEPOTS
+addappid(1885791, 1, "d00911c24fb4dac7e63b40c1d6d330818c628dd2976ef077a44e3f47e28f1db3") -- Depot 1885791
+setManifestid(1885791, "950773883648409578", 757955637)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
