aaac419d52e3c250fbc96de3004143fd7e3e7a52
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:33:18 +0700

    Tai moi Airlock Arena: Profit or Perish via Bot

---
+++ 1111920.lua
@@ -0,0 +1,12 @@
+-- 1111920's Lua and Manifest Created by Hubcap Manifest
+-- Airlock Arena
+-- Created: July 01, 2026 at 07:32:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1111920) -- Airlock Arena
+-- MAIN APP DEPOTS
+addappid(1111921, 1, "5248fada7c9886e04838675a5ad67d5f5cbbaa0c9195b0188799e1ee6c12d09e") -- Depot 1111921
+setManifestid(1111921, "4710370843949809301", 364174186)
