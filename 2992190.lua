698ea3049bad9c8bca9ec9dc0897a550a88898f5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-01 20:32:36 +0700

    Tai moi Blue Hunter via Bot

---
+++ 2992190.lua
@@ -0,0 +1,12 @@
+-- 2992190's Lua and Manifest Created by Hubcap Manifest
+-- Blue Hunter
+-- Created: July 01, 2026 at 07:33:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2992190) -- Blue Hunter
+-- MAIN APP DEPOTS
+addappid(2992192, 1, "2fcac5add167b30b33a44071fe3282f652d0f67c15d54234de0835dc6e750577") -- Depot 2992192
+setManifestid(2992192, "1944578794133565277", 333995584)
