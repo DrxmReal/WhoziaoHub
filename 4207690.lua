ed1bd3f107bf6e599354ad1cf409221d38b8b7c8
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:27 +0700

    Tai moi Dungeon Bomber via Bot

---
+++ 4207690.lua
@@ -0,0 +1,12 @@
+-- 4207690's Lua and Manifest Created by Hubcap Manifest
+-- Dungeon Bomber
+-- Created: July 02, 2026 at 00:52:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4207690) -- Dungeon Bomber
+-- MAIN APP DEPOTS
+addappid(4207691, 1, "63c31f1cdcce85176c9c5b9047925ffc2ca76856b09ee3b587866d6340861fd4") -- Depot 4207691
+setManifestid(4207691, "4234330681006809916", 486750781)
