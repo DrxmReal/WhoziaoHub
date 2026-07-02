0956a17ba9458b8be6756d8b169a4e51b8697944
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:00 +0700

    Tai moi Jaleco Sports: Goal! 2 via Bot

---
+++ 3399120.lua
@@ -0,0 +1,18 @@
+-- 3399120's Lua and Manifest Created by Hubcap Manifest
+-- Jaleco Sports: Goal! 2
+-- Created: July 02, 2026 at 05:01:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3399120) -- Jaleco Sports: Goal! 2
+-- MAIN APP DEPOTS
+addappid(3399121, 1, "c59f9cdcfc248fcd3fc31bf92d53c2e2a3326281b96e617acc92244d961ccf80") -- Depot 3399121
+setManifestid(3399121, "3069298492630459385", 406449013)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
