94cf05351fa051cc9c87c65d24b51546e1dabdc6
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 09:00:54 +0700

    Cap nhat PEAK via Bot

---
+++ 3527290.lua
@@ -0,0 +1,12 @@
+-- 3527290's Lua and Manifest Created by Hubcap Manifest
+-- PEAK
+-- Created: June 24, 2026 at 16:50:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3527290, 1, "499eb049b24a30730d5926e9cf72bfa9237d1a7f6de06e94a05e9c2c01348478") -- PEAK
+-- MAIN APP DEPOTS
+addappid(3527291, 1, "f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05") -- Depot 3527291
+setManifestid(3527291, "933519285287739916", 7796547299)
