f8d944687e687d3a041d63add0a3f0f349307c53
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:07 +0700

    Tai moi Tiny Slime Farm via Bot

---
+++ 4499040.lua
@@ -0,0 +1,16 @@
+-- 4499040's Lua and Manifest Created by Hubcap Manifest
+-- Tiny Slime Farm
+-- Created: July 02, 2026 at 05:00:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4499040) -- Tiny Slime Farm
+-- MAIN APP DEPOTS
+addappid(4499041, 1, "0680f29038fae6613ccd53d9608e38582a590228e8a3b9014e17a62a048b7859") -- Depot 4499041
+setManifestid(4499041, "2066254252321923676", 478447712)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
