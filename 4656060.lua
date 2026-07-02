909276035881e85d0d6e55ebc432c0dd478fb0ad
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:47 +0700

    Tai moi マイ・プラネット─MOON CHILD─ via Bot

---
+++ 4656060.lua
@@ -0,0 +1,12 @@
+-- 4656060's Lua and Manifest Created by Hubcap Manifest
+-- マイ・プラネット─MOON CHILD─
+-- Created: July 02, 2026 at 05:00:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4656060) -- マイ・プラネット─MOON CHILD─
+-- MAIN APP DEPOTS
+addappid(4656061, 1, "80778f31a4a0f0854755b760ba78357622a95e2805517d999cbdf3f55d8712f9") -- Depot 4656061
+setManifestid(4656061, "6485220193698740415", 586086498)
