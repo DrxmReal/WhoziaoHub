3bf084101c91b828bfaf32ff7f645a44479d6e3c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:23 +0700

    Tai moi Thief's Adventure via Bot

---
+++ 3877520.lua
@@ -0,0 +1,12 @@
+-- 3877520's Lua and Manifest Created by Hubcap Manifest
+-- Thief's Adventure
+-- Created: July 01, 2026 at 14:19:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3877520) -- Thief's Adventure
+-- MAIN APP DEPOTS
+addappid(3877521, 1, "ac4fe9ad270a88e5f8628f8000546a5e7ab7e2ee2dbcdba24ce568ecb1dfc1c7") -- Depot 3877521
+setManifestid(3877521, "6003471337640142539", 175090653)
