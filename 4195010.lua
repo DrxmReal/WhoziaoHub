b28af1de87ca5e696be8b40fc0e7ad9d8c46957f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:34 +0700

    Tai moi Which hand? via Bot

---
+++ 4195010.lua
@@ -0,0 +1,12 @@
+-- 4195010's Lua and Manifest Created by Hubcap Manifest
+-- Which hand?
+-- Created: July 02, 2026 at 06:04:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4195010, 1, "4cf168d4f2bf2951c4ab49d06b4b9626e529e61d5d8b0dd76fac75d0cb71e624") -- Which hand?
+-- MAIN APP DEPOTS
+addappid(4195011, 1, "3a9fed62c559fa827b4e01180685856d8295a117f513b8c1ad0e2291b69edac5") -- Depot 4195011
+setManifestid(4195011, "4153943183538657065", 207033228)
