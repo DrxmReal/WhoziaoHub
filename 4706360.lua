94ec798e8c043435dee6ae6a1e54340e43ef7ce8
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:57 +0700

    Tai moi PLAYBACK via Bot

---
+++ 4706360.lua
@@ -0,0 +1,18 @@
+-- 4706360's Lua and Manifest Created by Hubcap Manifest
+-- PLAYBACK
+-- Created: July 02, 2026 at 05:01:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4706360) -- PLAYBACK
+-- MAIN APP DEPOTS
+addappid(4706361, 1, "ed4d9de18b9e7c9eea48440334c725f49d79b5e32211f4f4a2a0b5a0e5aaa8f3") -- Depot 4706361
+setManifestid(4706361, "1755817434285695612", 8052019419)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
