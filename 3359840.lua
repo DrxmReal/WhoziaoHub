bea393d600948ee15602ec7cbf196a06b28d4376
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:04 +0700

    Tai moi Block Sprawl via Bot

---
+++ 3359840.lua
@@ -0,0 +1,12 @@
+-- 3359840's Lua and Manifest Created by Hubcap Manifest
+-- Block Sprawl
+-- Created: July 02, 2026 at 04:59:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3359840) -- Block Sprawl
+-- MAIN APP DEPOTS
+addappid(3359841, 1, "16f00fbc3b9239809477947976d041b23e2dd1ce7886d7e726793eb90654c7a9") -- Depot 3359841
+setManifestid(3359841, "8957991349394952688", 198499364)
