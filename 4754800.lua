821706c94dd54cf34a2f3daf0c63a8fa69b3313e
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 23:15:40 +0700

    Tai moi Aura Farming via Bot

---
+++ 4754800.lua
@@ -0,0 +1,12 @@
+-- 4754800's Lua and Manifest Created by Hubcap Manifest
+-- Aura Farming
+-- Created: June 25, 2026 at 04:25:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4754800) -- Aura Farming
+-- MAIN APP DEPOTS
+addappid(4754801, 1, "49f00675e64d4048f01b829b8a7433031e46ef8e04f816d5b43c5f2b37964783") -- Depot 4754801
+setManifestid(4754801, "6725666685389181698", 538990657)
