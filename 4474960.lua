9d8d5e140a91b10f083cdd1626ddf75465e294e1
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:23 +0700

    Tai moi Ratatouille Builder via Bot

---
+++ 4474960.lua
@@ -0,0 +1,12 @@
+-- 4474960's Lua and Manifest Created by Hubcap Manifest
+-- Ratatouille Builder
+-- Created: July 02, 2026 at 05:00:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4474960) -- Ratatouille Builder
+-- MAIN APP DEPOTS
+addappid(4474961, 1, "df27fa8989214be71b6e4d1a7996bb3811e348daf08af4dd241d887b84edbefc") -- Depot 4474961
+setManifestid(4474961, "8079026998845180498", 298851880)
