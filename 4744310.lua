9b3cce3feaaebe6f9676528f965a58f99ed012f6
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:04 +0700

    Tai moi Melancholy Lake - Camocipher12 Shorts via Bot

---
+++ 4744310.lua
@@ -0,0 +1,12 @@
+-- 4744310's Lua and Manifest Created by Hubcap Manifest
+-- Melancholy Lake
+-- Created: July 02, 2026 at 05:01:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4744310) -- Melancholy Lake
+-- MAIN APP DEPOTS
+addappid(4744311, 1, "f050440cd8a1592c9f0f5367cd088ecc3cb775d78a150b6f1eec1cc7568e2765") -- Depot 4744311
+setManifestid(4744311, "3679237284936349347", 216126226)
