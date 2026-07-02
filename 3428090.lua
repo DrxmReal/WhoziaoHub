b76c9f789f84a64aede53e6342a4894b8339ab83
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:42 +0700

    Tai moi Jaleco Sports: Bases Loaded 2 via Bot

---
+++ 3428090.lua
@@ -0,0 +1,18 @@
+-- 3428090's Lua and Manifest Created by Hubcap Manifest
+-- Jaleco Sports: Bases Loaded 2
+-- Created: July 02, 2026 at 06:04:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3428090) -- Jaleco Sports: Bases Loaded 2
+-- MAIN APP DEPOTS
+addappid(3428091, 1, "f02ed4c36d437cb120572f38dba0cc1a3b94c019ebc91c433b57d858fa539bdc") -- Depot 3428091
+setManifestid(3428091, "6272727424856767892", 463243561)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
