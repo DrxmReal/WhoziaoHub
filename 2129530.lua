099d991536d38f7cb6d0769d19afa4200caa3fb1
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 09:00:44 +0700

    Tai moi REANIMAL via Bot

---
+++ 2129530.lua
@@ -0,0 +1,27 @@
+-- 2129530's Lua and Manifest Created by Hubcap Manifest
+-- REANIMAL
+-- Created: April 24, 2026 at 09:00:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2129530, 1, "ea029714a30e4f9e526b5aa68792282d1ab1e7922fbba70da3983c9f958dcacb") -- REANIMAL
+-- MAIN APP DEPOTS
+addappid(2129531, 1, "8c619e3fd74002747f22974390275782dc2b568bc92b52859f0c5688a4a663f7") -- Depot 2129531
+setManifestid(2129531, "4243015198665777936", 15509476569)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- REANIMAL - Full Game (AppID: 4351720)
+addappid(4351720)
+addtoken(4351720, "7437698755938420611")
+addappid(4351720, 1, "6dba1be22c2161793226a2567e1ab5d6f153799494d7b26684d00e692ec041be") -- REANIMAL - Full Game - Depot 4351720
+setManifestid(4351720, "1736028932266953005", 15511917261)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3954470) -- REANIMAL - Foxhead and Muttonhead Masks
+addappid(4112550) -- REANIMAL  Season Pass
