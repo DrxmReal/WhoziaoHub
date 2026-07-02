dc8bb87761318d429ab21ed49b8cf5f369af34ba
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:04 +0700

    Tai moi Living in the Ending World via Bot

---
+++ 1424310.lua
@@ -0,0 +1,12 @@
+-- 1424310's Lua and Manifest Created by Hubcap Manifest
+-- Living in the Ending World
+-- Created: July 02, 2026 at 03:57:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1424310) -- Living in the Ending World
+-- MAIN APP DEPOTS
+addappid(1424311, 1, "83261018f7cfd20bc3636672c48a7047ef22149073d570bac5d069cf3911fc12") -- Living in the Ending World Content
+setManifestid(1424311, "873104278117589599", 83318337)
