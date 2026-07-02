5e8ef9b94d23939429549a264d7817144c3cf893
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:21 +0700

    Tai moi Kings Archer VR via Bot

---
+++ 4461420.lua
@@ -0,0 +1,20 @@
+-- 4461420's Lua and Manifest Created by Hubcap Manifest
+-- Kings Archer VR
+-- Created: July 02, 2026 at 05:00:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4461420) -- Kings Archer VR
+-- MAIN APP DEPOTS
+addappid(4461421, 1, "9f29c4c6281cd732e4e63c654564095f15374d654ba61f503191bd2a5ff690a2") -- Depot 4461421
+setManifestid(4461421, "1794775100637378504", 1423940917)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
