b265de03b86607ac670efe60358ec20c51f3a537
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:17 +0700

    Tai moi Duskvein Survivors - Bloodline via Bot

---
+++ 4759860.lua
@@ -0,0 +1,12 @@
+-- 4759860's Lua and Manifest Created by Hubcap Manifest
+-- Duskvein Survivors - Bloodline
+-- Created: July 02, 2026 at 06:04:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4759860) -- Duskvein Survivors - Bloodline
+-- MAIN APP DEPOTS
+addappid(4759861, 1, "3ea17eb2cc77e701b953f4a568f3a8b629b195440e27a8aadc52baf58abd0e60") -- Depot 4759861
+setManifestid(4759861, "711890867956498323", 947639487)
