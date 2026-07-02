a3c1a5607a62c4a3697e3ed95903bfe8637070d2
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:44 +0700

    Tai moi Body Pillow Royal via Bot

---
+++ 4747550.lua
@@ -0,0 +1,16 @@
+-- 4747550's Lua and Manifest Created by Hubcap Manifest
+-- Body Pillow Royal
+-- Created: July 02, 2026 at 05:00:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4747550) -- Body Pillow Royal
+-- MAIN APP DEPOTS
+addappid(4747551, 1, "d22641746c68c65be84d6e1704b53b02dad90658e90b332e76c5405ba6f0036f") -- Depot 4747551
+setManifestid(4747551, "6941447998307451753", 421336688)
+addappid(4747552, 1, "5d6532759667cc95885a3ae40ecc6a313a6f826abee54dc2cf72b176c0726d37") -- Depot 4747552
+setManifestid(4747552, "2493069577797760063", 412977428)
+addappid(4747553, 1, "b12db6d6aebadf3a520faf87ac4d3b830019029a09ee490e50842fb50bf0b904") -- Depot 4747553
+setManifestid(4747553, "2452531751902371557", 519940375)
