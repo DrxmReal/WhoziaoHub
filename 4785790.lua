9f9216a162695f2c4701b7b8bdaafcfaff450f56
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:04 +0700

    Tai moi 明末：甲申年 via Bot

---
+++ 4785790.lua
@@ -0,0 +1,13 @@
+-- 4785790's Lua and Manifest Created by Hubcap Manifest
+-- 明末：甲申年
+-- Created: July 02, 2026 at 11:41:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4785790) -- 明末：甲申年
+addtoken(4785790, "10643450807666841245")
+-- MAIN APP DEPOTS
+addappid(4785791, 1, "d7f1cd578280c8c5a17b6394181490d1e62e6355c0b3e292aa662e415d9a4986") -- Depot 4785791
+setManifestid(4785791, "8594977713129504342", 1018333408)
