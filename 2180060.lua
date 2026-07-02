5bb9b6d3654847d8531cffa0687678eb089ab19b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:10 +0700

    Tai moi Trouble in Tornado Town via Bot

---
+++ 2180060.lua
@@ -0,0 +1,12 @@
+-- 2180060's Lua and Manifest Created by Hubcap Manifest
+-- Trouble in Tornado Town
+-- Created: July 02, 2026 at 02:46:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2180060) -- Trouble in Tornado Town
+-- MAIN APP DEPOTS
+addappid(2180061, 1, "8dd10590e28aa250790217d93b33a1d7f16749eaf516c88a135ee1e39118e9e3") -- Depot 2180061
+setManifestid(2180061, "2372866902745153099", 1090941143)
