935f99dd41454449ae2da432d7f0fe8d1b83904c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:47 +0700

    Tai moi Star Devil via Bot

---
+++ 2082900.lua
@@ -0,0 +1,14 @@
+-- 2082900's Lua and Manifest Created by Hubcap Manifest
+-- Star Devil
+-- Created: July 02, 2026 at 06:04:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2082900) -- Star Devil
+-- MAIN APP DEPOTS
+addappid(2082901, 1, "5c3cee6293cecc31c06a3623cf4918875d58500e3f434d2c89bb81476461e751") -- Depot 2082901
+setManifestid(2082901, "5578176807688118653", 150350231)
+addappid(2082902, 1, "737667dd839a3721821e31077218a69b6d599c20678989d32f805b645ca25120") -- Depot 2082902
+setManifestid(2082902, "5051951067277466058", 137999257)
