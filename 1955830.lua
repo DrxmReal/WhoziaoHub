9d1ff2287d8b040f506ef34b10c0da6e3e6721e8
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 23:15:11 +0700

    Tai moi Touhou Hero of Ice Fairy via Bot

---
+++ 1955830.lua
@@ -0,0 +1,28 @@
+-- 1955830's Lua and Manifest Created by Hubcap Manifest
+-- Touhou Hero of Ice Fairy
+-- Created: June 27, 2026 at 15:22:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1955830, 1, "9f80378f9e7d4515184acc6a70674c027b05d931c4c0ab0b5dab56fd16895c7b") -- Touhou Hero of Ice Fairy
+-- MAIN APP DEPOTS
+addappid(1955831, 1, "7c520a8442f3537e7006593708adc76ceb19a2a0bdb9ad6a1158ad9d5c570dad") -- Depot 1955831
+setManifestid(1955831, "4293198447667612416", 4016734092)
+-- DLCS WITH DEDICATED DEPOTS
+-- Touhou Hero of Ice Fairy - Supporter Pack (AppID: 2302890)
+addappid(2302890)
+addappid(2302890, 1, "e1e6947c89bdac7882e1d6de61a59d18e5e1fa13688dc9df74db057d89fab786") -- Touhou Hero of Ice Fairy - Supporter Pack - Depot 2302890
+setManifestid(2302890, "1786412393826872683", 2731836462)
+-- Touhou Hero of Ice Fairy DLC1 - Rose Idol (AppID: 3213970)
+addappid(3213970)
+addappid(3213970, 1, "8aa8c519999b733e62801b312663af06939b9ca56e89c3fde921f995fc6f56a8") -- Touhou Hero of Ice Fairy DLC1 - Rose Idol - Depot 3213970
+setManifestid(3213970, "2570944442668429649", 997301128)
+-- Touhou Hero of Ice Fairy DLC2 - Immortal Phoenix And Lunarian Princess (AppID: 3858250)
+addappid(3858250)
+addappid(3858250, 1, "1b846efe1fb18615d3d59aded9675c77647f7fa67bf2f6ad90b1b0abecd46b88") -- Touhou Hero of Ice Fairy DLC2 - Immortal Phoenix And Lunarian Princess - Depot 3858250
+setManifestid(3858250, "7347670373316946976", 307051442)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4102150) -- DLC 4102150 (unreleased)
