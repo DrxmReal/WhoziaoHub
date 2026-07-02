78a36d0a4cbaaa03f6b532000e6b8ee344c440b9
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:30 +0700

    Tai moi Regret To Inform via Bot

---
+++ 4399130.lua
@@ -0,0 +1,12 @@
+-- 4399130's Lua and Manifest Created by Hubcap Manifest
+-- Regret To Inform
+-- Created: July 02, 2026 at 05:00:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4399130) -- Regret To Inform
+-- MAIN APP DEPOTS
+addappid(4399131, 1, "400c95eff9ed99f1c56f092a390b10cc1c89be505ddb928b4f13bad93cf9a7a4") -- Depot 4399131
+setManifestid(4399131, "1663773419368509121", 252852943)
