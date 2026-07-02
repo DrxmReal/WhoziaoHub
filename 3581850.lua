ccb4c611335c04b0cf5704590a49e1386e27f2b4
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:20 +0700

    Tai moi Diamond Runway via Bot

---
+++ 3581850.lua
@@ -0,0 +1,12 @@
+-- 3581850's Lua and Manifest Created by Hubcap Manifest
+-- Diamond Runway
+-- Created: July 01, 2026 at 19:10:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3581850, 1, "70b637b242362a5ba7e742e92fc2714c69e6b97ced1efebdeba2aa0ffcbf8ac1") -- Diamond Runway
+-- MAIN APP DEPOTS
+addappid(3581851, 1, "0ed5d37d2d8fb591aa1140814c46444155b838d89aa8dd8926fdf7919b711634") -- Depot 3581851
+setManifestid(3581851, "9037649927879791388", 1373248531)
