cb32658be9dc1c38042b8dd193def08803cbd7ba
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 23:15:25 +0700

    Tai moi Shattered Divinities via Bot

---
+++ 3418990.lua
@@ -0,0 +1,12 @@
+-- 3418990's Lua and Manifest Created by Hubcap Manifest
+-- 神陨之地 Shattered Divinities
+-- Created: June 03, 2026 at 10:44:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3418990) -- 神陨之地 Shattered Divinities
+-- MAIN APP DEPOTS
+addappid(3418991, 1, "42df9079dedc1fc0173d9f6326ec3e832908c5f6398d1d4d106f5a8db311c4bb") -- Depot 3418991
+setManifestid(3418991, "7467746590067309121", 2835570493)
