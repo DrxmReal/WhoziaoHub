526de33e19166e94780660b15d5343b7704fdf2b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:24 +0700

    Tai moi Extreme Cycling Championship via Bot

---
+++ 4688690.lua
@@ -0,0 +1,18 @@
+-- 4688690's Lua and Manifest Created by Hubcap Manifest
+-- Extreme Cycling Championship
+-- Created: July 02, 2026 at 06:04:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4688690) -- Extreme Cycling Championship
+-- MAIN APP DEPOTS
+addappid(4688691, 1, "eb7bb39a70e232ba83235762ef3ff24a46afe48ad7f73c9c485360d8b6511793") -- Depot 4688691
+setManifestid(4688691, "3189410531745469109", 2431575897)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
