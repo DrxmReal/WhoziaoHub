9fa47ff5a5312e84b0b3d890a99e4f575e9678f3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:11 +0700

    Tai moi Scelerophobia via Bot

---
+++ 4839450.lua
@@ -0,0 +1,12 @@
+-- 4839450's Lua and Manifest Created by Hubcap Manifest
+-- Scelerophobia
+-- Created: July 02, 2026 at 05:00:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4839450) -- Scelerophobia
+-- MAIN APP DEPOTS
+addappid(4839451, 1, "ef41950e4b42bd221150b5899283fb05b78ad9f7e94ffb339b179ba201de9bce") -- Depot 4839451
+setManifestid(4839451, "7750249190952923294", 346566147)
