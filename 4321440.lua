daf7978cdea89aa847d9348aeeca2823ef26af78
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:52 +0700

    Tai moi Mazemory via Bot

---
+++ 4321440.lua
@@ -0,0 +1,12 @@
+-- 4321440's Lua and Manifest Created by Hubcap Manifest
+-- Mazemory
+-- Created: July 02, 2026 at 04:59:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4321440) -- Mazemory
+-- MAIN APP DEPOTS
+addappid(4321441, 1, "d2be1607fbbc0b520d5ec38222d7a6c514e2a4a7a855b7a723687717f7aaae31") -- Depot 4321441
+setManifestid(4321441, "1150193556951244026", 701512369)
