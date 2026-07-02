73989c692a600eaa40de30fe2119327c9d3380da
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:40:08 +0700

    Tai moi Breeding Estate via Bot

---
+++ 4273060.lua
@@ -0,0 +1,14 @@
+-- 4273060's Lua and Manifest Created by Hubcap Manifest
+-- タネヅケ不動産～赤ちゃんの為の子宮（ぶっけん）探し～
+-- Created: July 02, 2026 at 06:27:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4273060) -- タネヅケ不動産～赤ちゃんの為の子宮（ぶっけん）探し～
+-- MAIN APP DEPOTS
+addappid(4273061, 1, "f9b0921ab1dd43aa4df81e287b63fee4033a0a75f4a50c01e3372742984b9ca8") -- Depot 4273061
+setManifestid(4273061, "3183884321174269596", 1651097459)
+addappid(4273062, 1, "11a0f3a313c73e43c728a6f4c6f62f850508444e6b59920356a62a0d4952011e") -- Depot 4273062
+setManifestid(4273062, "6082414939801355513", 1645008437)
