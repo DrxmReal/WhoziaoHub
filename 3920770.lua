23fa3b885d120a8fda8e1163f7dd82531cf2d499
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:42:16 +0700

    Tai moi Girls' Dorm Wars via Bot

---
+++ 3920770.lua
@@ -0,0 +1,12 @@
+-- 3920770's Lua and Manifest Created by Hubcap Manifest
+-- Girls' Dorm Wars
+-- Created: July 01, 2026 at 19:46:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3920770, 1, "d0fe55104db998ad1605cf23e88add5d8fd24421523b96e2af0360765c1dd8f0") -- Girls' Dorm Wars
+-- MAIN APP DEPOTS
+addappid(3920771, 1, "d3004892444a8cdcfc08c92e2331ecc9a2afb9fdeddca39307a95a6227c1b59a") -- Depot 3920771
+setManifestid(3920771, "5739287249173125862", 515208168)
