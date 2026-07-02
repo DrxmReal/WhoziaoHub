8877f3563119c2012b8e67d35cb0fb53e4c395bb
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-02 22:41:56 +0700

    Tai moi The Host's deadly games via Bot

---
+++ 4454200.lua
@@ -0,0 +1,12 @@
+-- 4454200's Lua and Manifest Created by Hubcap Manifest
+-- The Host's deadly games
+-- Created: July 02, 2026 at 04:59:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4454200) -- The Host's deadly games
+-- MAIN APP DEPOTS
+addappid(4454201, 1, "cd1b7c1a9e0e7cf2753100ef9cc5fd9f875482374125257fcba21d3f23155f67") -- Depot 4454201
+setManifestid(4454201, "553118142476699536", 2868518309)
