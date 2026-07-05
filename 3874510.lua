25a84b4e5f40b9eb9180f14c8238ed7acc6189c8
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 10:45:41 +0700

    Tai moi Nightmare Mines via Bot

---
+++ 3874510.lua
@@ -0,0 +1,14 @@
+-- 3874510's Lua and Manifest Created by Hubcap Manifest
+-- Nightmare Mines
+-- Created: July 04, 2026 at 22:51:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3874510) -- Nightmare Mines
+-- MAIN APP DEPOTS
+addappid(3874511, 1, "f8472e2fca36accad0e2e81dfc456725aca3e65cbd7fa0991de8798ab674a373") -- Depot 3874511
+setManifestid(3874511, "5911141309625286225", 143320769)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3894190) -- Nightmare Mines - Board Vip
