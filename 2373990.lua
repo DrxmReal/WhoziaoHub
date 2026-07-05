9ddc3407df4a76f3b56a40a143405458ef247be2
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 10:42:57 +0700

    Tai moi Solo Leveling: ARISE OVERDRIVE via Bot

---
+++ 2373990.lua
@@ -0,0 +1,20 @@
+-- 2373990's Lua and Manifest Created by Hubcap Manifest
+-- Solo Leveling: ARISE OVERDRIVE
+-- Created: June 25, 2026 at 21:15:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 3
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2373990, 1, "907973645448aa77cf21b57183812bea5a58a29df75bfc0a4dc37bc6bdff0371") -- Solo Leveling: ARISE OVERDRIVE
+-- MAIN APP DEPOTS
+addappid(2373991, 1, "5a97586d39bdb2a503376a75e3cae7d1f7e2de1bcb9ced523a73b21ca79de413") -- Depot 2373991
+setManifestid(2373991, "1040043721404863303", 48162201183)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3638570) -- Solo LevelingARISE OVERDRIVE - Deluxe Edition
+addappid(4240370) -- Solo LevelingARISE OVERDRIVE - Christmas Costume Gift Pack
+addappid(4710940) -- Solo LevelingARISE OVERDRIVE - Jeju Island Raid Special Pack
