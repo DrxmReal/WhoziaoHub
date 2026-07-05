e8c61aedb8c32363930204061db4ed2356ba3053
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 12:43:49 +0700

    Tai moi Five Nights at Freddy's: Security Breach via Bot

---
+++ 747660.lua
@@ -0,0 +1,23 @@
+-- 747660's Lua and Manifest Created by Hubcap Manifest
+-- Five Nights at Freddy's: Security Breach
+-- Created: September 29, 2025 at 12:34:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(747660) -- Five Nights at Freddy's: Security Breach
+-- MAIN APP DEPOTS
+addappid(747661, 1, "2be8f0b84ce1709f94c1e3f51780dc5d9b578dee06c6e2a01bfa8062612b39d7") -- Quarters Content
+setManifestid(747661, "5415406360052716797", 68972616659)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Five Nights at Freddys Security Breach - Ruin (AppID: 1924720)
+addappid(1924720)
+addappid(1924720, 1, "0082af8a55cb6a77b1d8de8fc5264b5ec9899864bd449a859cffbf97f0125859") -- Five Nights at Freddys Security Breach - Ruin - Five Nights at Freddy's: Security Breach - Chowda (1924720) Depot
+setManifestid(1924720, "4610443762425291873", 26842196574)
