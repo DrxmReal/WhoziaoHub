b3555619ad61c2a067d8bed1b8a4fba46617b90d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 15:19:35 +0700

    Tai moi Candylands Journey via Bot

---
+++ 3667100.lua
@@ -0,0 +1,12 @@
+-- 3667100's Lua and Manifest Created by Hubcap Manifest
+-- Candylands Journey
+-- Created: July 04, 2026 at 22:50:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3667100) -- Candylands Journey
+-- MAIN APP DEPOTS
+addappid(3667101, 1, "f15831230cdcf2c21d0ec8004f2e9e7c2f9b031f5b0c17128442658a79b22314") -- Depot 3667101
+setManifestid(3667101, "1126459596473621153", 260661143)
