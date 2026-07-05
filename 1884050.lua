330afbcecad1ffee6089cbde905d4f3327fd4dfa
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 15:19:25 +0700

    Tai moi Annual Intruders 2.0 via Bot

---
+++ 1884050.lua
@@ -0,0 +1,12 @@
+-- 1884050's Lua and Manifest Created by Hubcap Manifest
+-- Annual Intruders 2.0
+-- Created: July 04, 2026 at 22:51:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1884050) -- Annual Intruders 2.0
+-- MAIN APP DEPOTS
+addappid(1884051, 1, "8ce4fc60a8d663b97afcf0735de6b73342f7de9ab98f0899ed2430e8eace1b2d") -- Depot 1884051
+setManifestid(1884051, "5366627774292934556", 1200432094)
