0d85f11aaecf2d5d853a2414cac8cba5acd9eedd
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 10:43:02 +0700

    Tai moi Afallon via Bot

---
+++ 2597810.lua
@@ -0,0 +1,12 @@
+-- 2597810's Lua and Manifest Created by Hubcap Manifest
+-- Afallon
+-- Created: July 04, 2026 at 23:42:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2597810) -- Afallon
+-- MAIN APP DEPOTS
+addappid(2597811, 1, "084e7d51d514f3a5b9d28a0fb64f6e7e0957abd73ec2a03691f3b331a14687ff") -- Depot 2597811
+setManifestid(2597811, "4557531716572407424", 22216431297)
