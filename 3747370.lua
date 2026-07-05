6bee97214faceb706f9d34d684516e4d17f5f6d5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 10:45:31 +0700

    Tai moi CARDnal DESIRES via Bot

---
+++ 3747370.lua
@@ -0,0 +1,12 @@
+-- 3747370's Lua and Manifest Created by Hubcap Manifest
+-- CARDnal DESIRES
+-- Created: July 04, 2026 at 22:52:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3747370, 1, "301cc03c61249c5f67b5af047428c03a615583ca86f26f9d8ea361e28b9629c1") -- CARDnal DESIRES
+-- MAIN APP DEPOTS
+addappid(3747371, 1, "a2dfb61a86f70548f9863a4b7479174618034dff79e83e3493fa43a3e67805cf") -- Depot 3747371
+setManifestid(3747371, "352994213609594787", 265705072)
