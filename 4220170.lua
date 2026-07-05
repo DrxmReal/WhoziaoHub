b1c0a67d766754e05b6ce3e2e6952b5e8bc460c3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-05 10:45:37 +0700

    Tai moi Batamonisk via Bot

---
+++ 4220170.lua
@@ -0,0 +1,12 @@
+-- 4220170's Lua and Manifest Created by Hubcap Manifest
+-- Batamonisk
+-- Created: July 04, 2026 at 22:52:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4220170) -- Batamonisk
+-- MAIN APP DEPOTS
+addappid(4220172, 1, "a3881a13a56fbb793d86a5f08fad695d261c35cbaa0af364affea569c0047d84") -- Depot 4220172
+setManifestid(4220172, "3599242875637837183", 250791864)
