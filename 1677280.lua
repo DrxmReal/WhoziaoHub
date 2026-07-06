9a2553c6176e31d0a95a3ace829aa83c7bf3f889
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-07 00:15:31 +0700

    Tai moi Company of Heroes 3 via Bot

---
+++ 1677280.lua
@@ -0,0 +1,39 @@
+-- 1677280's Lua and Manifest Created by Hubcap Manifest
+-- Company of Heroes 3
+-- Created: July 04, 2026 at 10:44:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 8
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1677280, 1, "3bc2162aacc5a2b9978b30069dffbbd4e0dcedfde3b7db80768525428e963600") -- Company of Heroes 3
+-- MAIN APP DEPOTS
+addappid(1677281, 1, "946c1091854e4b36a1468dea11adb8fe4fe6db6485976132a50781a70d5a206e") -- Depot 1677281
+setManifestid(1677281, "7391821069624589291", 33283193576)
+addappid(1677282, 1, "2dbe642c09395dcb0cfab68ff34eca066c3f47e38d8511d59509fb60cf7c665a") -- Depot 1677282
+setManifestid(1677282, "83851306951263697", 4468)
+addappid(1677283, 1, "532412e59c494bd36e8efc9d4e257208e7412c8a70b16f206103f3913d4add50") -- Depot 1677283
+setManifestid(1677283, "6058592591375980183", 7783)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2068340) -- Company of Heroes 3 - Pre Order Content
+addappid(2068341) -- Company of Heroes 3 - Premium Content
+addappid(2068342) -- Company of Heroes 3 Hammer  Shield Expansion Pack
+addappid(3300460) -- Company of Heroes 3 Stealth  Stronghold
+addappid(3469800) -- Company of Heroes 3 Fire  Steel
+addappid(4095900) -- Company of Heroes 3 Endure  Defy
+addappid(4523180) -- Company of Heroes 3 Hammer  Shield Battlegroup DLC
+addappid(4523380) -- Company of Heroes 3 Dare  Destroy
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1677284) -- Depot 1677284 (empty depot)
+-- addappid(1677285) -- Depot 1677285 (empty depot)
+-- addappid(1677286) -- Depot 1677286 (empty depot)
+-- addappid(1677287) -- Depot 1677287 (empty depot)
+-- addappid(1677289) -- Depot 1677289 (empty depot)
