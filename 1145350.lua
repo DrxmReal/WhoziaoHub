5d79a418c2f2d691338eee02b010854ea9f4b3b5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-06 22:42:54 +0700

    Tai moi Hades II via Bot

---
+++ 1145350.lua
@@ -0,0 +1,20 @@
+-- 1145350's Lua and Manifest Created by Hubcap Manifest
+-- Hades II
+-- Created: June 10, 2026 at 12:57:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1145350, 1, "b4b04fea10664cb3ea0d6e6f8876e3350ed60657be95ed215f7d9795036dd11f") -- Hades II
+-- MAIN APP DEPOTS
+addappid(1145352, 1, "ff02298666a5470ddca19f89593983136f6d1727fc82957ca6b847133783b4e7") -- Depot 1145352
+setManifestid(1145352, "5047543936178947708", 11587115468)
+addappid(1145354, 1, "71f0364cb7bd2b7d7755605c7983bf2b73101a6e2e0ef6b7082d275a4650f7e5") -- Depot 1145354
+setManifestid(1145354, "7476980192732998655", 186878040)
+addappid(1145355, 1, "67def0278f458945e71a75914c686e5bf4d3f90c8b4320795c3b78ad999afc56") -- Depot 1145355
+setManifestid(1145355, "4248563244724666705", 11732024723)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
