e9eb50d952ad7456a8bcd3d6a1501599cff388f5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-07 11:35:57 +0700

    Tai moi Broken Arrow via Bot

---
+++ 1604270.lua
@@ -0,0 +1,43 @@
+-- 1604270's Lua and Manifest Created by Hubcap Manifest
+-- Broken Arrow
+-- Created: July 03, 2026 at 15:08:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 12
+-- Total DLCs: 2
+-- Shared Depots: 9
+
+-- MAIN APPLICATION
+addappid(1604270, 1, "6a1d71140c5c5a62ae105124fe68479f68eb01b0bd07c51ce511632f9101ec2c") -- Broken Arrow
+addtoken(1604270, "1075697477965442338")
+-- MAIN APP DEPOTS
+addappid(1604271, 1, "da1386311fdfb5dc5fd71357c294bbce7afd7e86bffd0588249f39807ac91d08") -- Depot 1604271
+setManifestid(1604271, "1350642488699410226", 60107782431)
+addappid(1604272, 1, "dec4f347ca36d0293362a8dba7e62b9d0f950872215aa4b70de99549ad402b27") -- Depot 1604272
+setManifestid(1604272, "4341980721484989447", 3977472)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+setManifestid(229000, "4622705914179893434", 242743889)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") -- .NET 3.5 Client Profile Redist (Shared from App 228980)
+setManifestid(229001, "4049573910112143457", 267964564)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") -- .NET 4.0 Redist (Shared from App 228980)
+setManifestid(229002, "7260605429366465749", 50450161)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- .NET 4.0 Client Profile Redist (Shared from App 228980)
+setManifestid(229003, "8740933542064151477", 43001447)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- .NET 4.5.2 Redist (Shared from App 228980)
+setManifestid(229004, "5220958916987797232", 70000464)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+-- DLCS WITH DEDICATED DEPOTS
+-- Broken Arrow Vanguard Edition DLC (AppID: 3751200)
+addappid(3751200)
+addtoken(3751200, "11570295912332125767")
+addappid(3751200, 1, "7da9c745d0ff4b29d80193b0310c863c94993cf694eb5fb9c815e2a808e58e35") -- Broken Arrow Vanguard Edition DLC - Depot 3751200
+setManifestid(3751200, "7329335284774443223", 75202221)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4151720) -- Broken Arrow Baltic Battalion
