2327fffbb877bf90318f41edbdaa62076d567cc0
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-08 12:48:05 +0700

    Tai moi PICO PARK 2 via Bot

---
+++ 2644470.lua
@@ -0,0 +1,14 @@
+-- 2644470's Lua and Manifest Created by Hubcap Manifest
+-- PICO PARK 2
+-- Created: September 30, 2025 at 06:35:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2644470) -- PICO PARK 2
+-- MAIN APP DEPOTS
+addappid(2644471, 1, "742488856b65c1f5c8c1d8c3d8b64f0f47fe89887d5308427c354d9ef5902e51") -- Depot 2644471
+setManifestid(2644471, "977062023767142031", 217514181)
+addappid(2644472, 1, "b53539a4f7c49575c61018fe492c2d05dbb6ca16267bc077043797ef23f427f6") -- Depot 2644472
+setManifestid(2644472, "880294164879156752", 309892636)
