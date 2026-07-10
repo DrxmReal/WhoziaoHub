e6dc2f11e3643785b2cfebc3e53f50430f6de512
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-10 22:33:21 +0700

    Tai moi Cozy Island via Bot

---
+++ 2918500.lua
@@ -0,0 +1,14 @@
+-- 2918500's Lua and Manifest Created by Hubcap Manifest
+-- Cozy Island
+-- Created: April 13, 2026 at 02:17:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2918500, 1, "8451db99ade9f235a6024ca134cdb884c0455f81e77f1ce946036b051290961c") -- Cozy Island
+-- MAIN APP DEPOTS
+addappid(2918501, 1, "444ce90f8665148c49ad7c254f7729d6e85e823d967a915c0e2350f887d1245f") -- Depot 2918501
+setManifestid(2918501, "3634896321659563288", 909605451)
+addappid(2918502, 1, "5079802e934edb95e5b43f7c17d5a078510b6987c66100f8f17c9587dcfb3c8c") -- Depot 2918502
+setManifestid(2918502, "984473028653078787", 902698276)
