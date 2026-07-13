bcc47b9ecaf62af1430b05e4f4f060b119264416
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-13 14:30:03 +0700

    Tai moi Palworld via Bot

---
+++ 1623730.lua
@@ -0,0 +1,18 @@
+-- 1623730's Lua and Manifest Created by Hubcap Manifest
+-- Palworld
+-- Created: July 09, 2026 at 23:41:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1623730, 1, "214e5bcf1036d2bc62193cf2bc15e148899db0ed4799c3a2c0d41f2490d136f5") -- Palworld
+-- MAIN APP DEPOTS
+addappid(1623731, 1, "7a422988135296505205dd489945d4b17ed7414734dd627744adac84aade7b73") -- Depot 1623731
+setManifestid(1623731, "1208219330428218979", 41185052542)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
