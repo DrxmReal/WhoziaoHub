5de25d2d110def5e088e06ef99e1788bdbd2e1cb
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-21 12:26:01 +0700

    Tai moi The Hust Banhmi | Bánh Mì Bách Khoa via Bot

---
+++ 4530780.lua
@@ -0,0 +1,12 @@
+-- 4530780's Lua and Manifest Created by Hubcap Manifest
+-- The Hust Banhmi | Bánh Mì Bách Khoa
+-- Created: July 20, 2026 at 07:18:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4530780) -- The Hust Banhmi | Bánh Mì Bách Khoa
+-- MAIN APP DEPOTS
+addappid(4530781, 1, "50dabdba70d17dba26d19792a485ad4867ffd67990b309144cfab801be9602d0") -- Depot 4530781
+setManifestid(4530781, "718200557868434523", 4785480900)
