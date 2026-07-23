a186c8f3b982d5541d4c1e88c332dbc371f325a7
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-23 21:19:08 +0700

    Tai moi Scritchy Scratchy via Bot

---
+++ 3948120.lua
@@ -0,0 +1,16 @@
+-- 3948120's Lua and Manifest Created by Hubcap Manifest
+-- Scritchy Scratchy
+-- Created: June 03, 2026 at 15:51:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3948120, 1, "2972cbb18aae51595ed6a585a9bb56c70060687d21ff3802cecb5c79fcc1de60") -- Scritchy Scratchy
+-- MAIN APP DEPOTS
+addappid(3948121, 1, "4d5ba58100eba881f6f9a66ee5982857f3db6a375f5319bdfddd6d9b71defc07") -- Depot 3948121
+setManifestid(3948121, "2568898334237591722", 441146691)
+addappid(3948122, 1, "328927d058e2934b073e34b22bcbf5103daa2dea1879877f2ef930f2cf7ef046") -- Depot 3948122
+setManifestid(3948122, "5819585551787439752", 548377640)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4428640) -- Scritchy Scratchy - Supporter Pack
