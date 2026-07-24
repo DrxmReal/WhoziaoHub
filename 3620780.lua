1c894e63e658410bbdd2d5d6e9a045ccdd144614
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-24 21:26:22 +0700

    Tai moi AI-deal-Rays [General Edition] via Bot

---
+++ 3620780.lua
@@ -0,0 +1,17 @@
+-- 3620780's Lua and Manifest Created by Hubcap Manifest
+-- AI-deal-Rays
+-- Created: May 19, 2026 at 08:51:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3620780, 1, "6dc36b6048c66b7ba645284fdde9b2f51f4e8f28e03cf5e546b3351d08f1b35c") -- AI-deal-Rays
+-- MAIN APP DEPOTS
+addappid(3620781, 1, "f12ac6abfda9bcf2d85eb2f81ee1efbfb313afe57e458694b144d11a937a2ba2") -- Depot 3620781
+setManifestid(3620781, "6054473792289445222", 15839714663)
+-- DLCS WITH DEDICATED DEPOTS
+-- AI-deal-Rays - Adult Content Pack (AppID: 4006900)
+addappid(4006900)
+addappid(4006901, 1, "4f59056c67199cb0e77329eff582492c162099be08abfde7d1dae708bd286e7d") -- AI-deal-Rays - Adult Content Pack - Depot 4006901
+setManifestid(4006901, "629864698001123903", 825621234)
