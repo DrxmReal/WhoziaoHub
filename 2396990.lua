93b70466edb6ad53116a9ba82490fee5da3936b3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-24 21:26:16 +0700

    Tai moi Fate/hollow ataraxia REMASTERED via Bot

---
+++ 2396990.lua
@@ -0,0 +1,16 @@
+-- 2396990's Lua and Manifest Created by Hubcap Manifest
+-- Fate/hollow ataraxia REMASTERED
+-- Created: November 02, 2025 at 20:33:31 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2396990) -- Fate/hollow ataraxia REMASTERED
+-- MAIN APP DEPOTS
+addappid(2396991, 1, "5100b046f94ce318bd23122f91c9d90b4a0e8c0e873bee7bde1cea354300cc52") -- Depot 2396991
+setManifestid(2396991, "4408487838010541422", 8240667541)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
