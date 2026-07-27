83410e6acc839f40b62820c3f38c6efda7a89578
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:52:06 +0700

    Tai moi Sunny Side Up via Bot

---
+++ 4267080.lua
@@ -0,0 +1,20 @@
+-- 4267080's Lua and Manifest Created by Hubcap Manifest
+-- Sunny Side Up
+-- Created: July 26, 2026 at 23:29:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4267080) -- Sunny Side Up
+-- MAIN APP DEPOTS
+addappid(4267081, 1, "d8414ee3b34b80aea662a041e6345d179450e4773d8606f392b9de83f6404abe") -- Depot 4267081
+setManifestid(4267081, "1167603677114844421", 1103098388)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4267082) -- Depot 4267082 (empty depot)
