087a81bb11ea4aebdffce6d3a0f11205c66ac736
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:48:46 +0700

    Tai moi Road Diner Simulator: Prologue via Bot

---
+++ 3199870.lua
@@ -0,0 +1,12 @@
+-- 3199870's Lua and Manifest Created by Hubcap Manifest
+-- Road Diner Simulator: Prologue
+-- Created: July 27, 2026 at 09:59:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3199870) -- Road Diner Simulator: Prologue
+-- MAIN APP DEPOTS
+addappid(3199871, 1, "f988faa7cd82e70beb09038eae66dd023f382c1425a57fe0f3577adf00624c4b") -- Depot 3199871
+setManifestid(3199871, "4181023322114223100", 2819996665)
