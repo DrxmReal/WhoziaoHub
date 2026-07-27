4f5a352e2f53986578f7adbed832b21a34c22e6f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:48:49 +0700

    Tai moi Infinite Ruins via Bot

---
+++ 4317900.lua
@@ -0,0 +1,12 @@
+-- 4317900's Lua and Manifest Created by Hubcap Manifest
+-- Infinite Ruins
+-- Created: July 27, 2026 at 09:03:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4317900) -- Infinite Ruins
+-- MAIN APP DEPOTS
+addappid(4317901, 1, "f3c3a8afc635ebf372aa867b7cf8318cac3b575710c89d02fa98a26b16e2502d") -- Depot 4317901
+setManifestid(4317901, "2767270724825221074", 252717902)
