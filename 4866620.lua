ddb79e1152532cbb09907a4c8744f444c31233b0
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:49:44 +0700

    Tai moi Transhumance via Bot

---
+++ 4866620.lua
@@ -0,0 +1,12 @@
+-- 4866620's Lua and Manifest Created by Hubcap Manifest
+-- transhumance
+-- Created: July 27, 2026 at 04:40:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4866620) -- transhumance
+-- MAIN APP DEPOTS
+addappid(4866621, 1, "ac16d01d09dd4fa828d8524bca53a15c999d9c0aff713c3fb4ce8250d40dff54") -- Depot 4866621
+setManifestid(4866621, "7140814794944283909", 184184864)
