5b3b982ba66405dc83037e37c28c68df9d9f777a
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:50:36 +0700

    Tai moi Frog Galaxy Survivor via Bot

---
+++ 3083360.lua
@@ -0,0 +1,19 @@
+-- 3083360's Lua and Manifest Created by Hubcap Manifest
+-- Frog Galaxy Survivor
+-- Created: July 27, 2026 at 01:19:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3083360) -- Frog Galaxy Survivor
+addtoken(3083360, "4683366492477291343")
+-- MAIN APP DEPOTS
+addappid(3083361, 1, "a1ff47ad19ac0a499fff4a42bd518fedc0cdf521729d979b4cec2a57da062ef4") -- Depot 3083361
+setManifestid(3083361, "2537013797458500809", 3281237464)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
