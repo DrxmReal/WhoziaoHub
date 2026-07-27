b261b27ba17ef6a829ef238477abfc4d0c43c527
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:50:56 +0700

    Tai moi Unknown Game (1030280) via Bot

---
+++ 1030280.lua
@@ -0,0 +1,15 @@
+-- 1030280's Lua and Manifest Created by Hubcap Manifest
+-- 翔的堕落人生
+-- Created: July 27, 2026 at 01:17:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1030280) -- 翔的堕落人生
+addtoken(1030280, "6399048119979734585")
+-- MAIN APP DEPOTS
+addappid(1030281, 1, "a1e67984dd15e085a02f71ec85c1df4934511dc283d17f88d90fd1a710c02948") -- Depot 1030281
+setManifestid(1030281, "213158135860546442", 280886389)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1030282) -- Depot 1030282 (empty depot)
