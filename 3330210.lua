079d450d5fa679fa2f472c069f3050111a9956ef
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:49:01 +0700

    Tai moi Kinny and the Cosmic Cauldron via Bot

---
+++ 3330210.lua
@@ -0,0 +1,12 @@
+-- 3330210's Lua and Manifest Created by Hubcap Manifest
+-- Kinny and the Cosmic Cauldron
+-- Created: July 27, 2026 at 10:48:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3330210, 1, "e35260031233aeeb8f0f282eac7153a7a97a5316b21e0f7e9812ed2b9e9df655") -- Kinny and the Cosmic Cauldron
+-- MAIN APP DEPOTS
+addappid(3330211, 1, "28ab538037e2ea307c5b89b61c64697153cff9b28e1b0d75f5790e90b61a1d0d") -- Depot 3330211
+setManifestid(3330211, "629632338752499518", 1528613231)
