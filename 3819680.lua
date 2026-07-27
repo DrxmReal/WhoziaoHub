4dbcf5729cc18aa5992df54cd182c4c55c510f0c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:49:52 +0700

    Tai moi Little One via Bot

---
+++ 3819680.lua
@@ -0,0 +1,12 @@
+-- 3819680's Lua and Manifest Created by Hubcap Manifest
+-- Little One
+-- Created: July 27, 2026 at 04:40:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3819680) -- Little One
+-- MAIN APP DEPOTS
+addappid(3819681, 1, "9ac18b0f3dde1a9293ccb06f24293e2ef1d890c3776565aa5fbdc5dd69cf8fb6") -- Depot 3819681
+setManifestid(3819681, "3292350835313069607", 274797790)
