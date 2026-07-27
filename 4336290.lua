0b334391a9f77a03381c75b7cbf56520ed9dcdd5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:48:59 +0700

    Tai moi Tyrania Enforcers - Visual Novel via Bot

---
+++ 4336290.lua
@@ -0,0 +1,16 @@
+-- 4336290's Lua and Manifest Created by Hubcap Manifest
+-- Tyrania Enforcers - Visual Novel
+-- Created: July 27, 2026 at 10:08:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4336290) -- Tyrania Enforcers - Visual Novel
+-- MAIN APP DEPOTS
+addappid(4336291, 1, "943928dfd1953cbd29ce22a9ce7e2bb868ee1b2c238e53cbac15e2837001ded2") -- Depot 4336291
+setManifestid(4336291, "3477664807457186576", 1894340117)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Tyrania Enforcers - Wallpapers DLC (AppID: 4984670) - missing depot keys
+-- addappid(4984670)
