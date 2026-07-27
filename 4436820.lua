d718efbf582ec95d1d9c8eec35396d3de2160c5d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:52:18 +0700

    Tai moi Deserted Island Where Desire Blossoms via Bot

---
+++ 4436820.lua
@@ -0,0 +1,12 @@
+-- 4436820's Lua and Manifest Created by Hubcap Manifest
+-- Deserted Island Where Desire Blossoms
+-- Created: July 26, 2026 at 08:07:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4436820) -- Deserted Island Where Desire Blossoms
+-- MAIN APP DEPOTS
+addappid(4436821, 1, "98de17092df8c6d1aaf94100076493606293d1af95d399022027debb23399b0e") -- Depot 4436821
+setManifestid(4436821, "3607836030644303937", 1188372437)
