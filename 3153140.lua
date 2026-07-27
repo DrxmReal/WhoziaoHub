2b001c049ac77020afc9cd7f7b3d537ea6752a86
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:50:20 +0700

    Tai moi Mush Dash via Bot

---
+++ 3153140.lua
@@ -0,0 +1,18 @@
+-- 3153140's Lua and Manifest Created by Hubcap Manifest
+-- Mush Dash
+-- Created: July 27, 2026 at 04:27:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3153140) -- Mush Dash
+-- MAIN APP DEPOTS
+addappid(3153141, 1, "b31a8152efadfc667623724825f6927bbfd96882d995f62432e1ee6f25675fb3") -- Depot 3153141
+setManifestid(3153141, "3806607269869939960", 1249194018)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
