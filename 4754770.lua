6adeb3c93285751cf9f33684b63bfd7065e89d79
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:51:33 +0700

    Tai moi ​Hands Off the Priest! My Creditor is a Telepathic Pervert via Bot

---
+++ 4754770.lua
@@ -0,0 +1,20 @@
+-- 4754770's Lua and Manifest Created by Hubcap Manifest
+-- ​Hands Off the Priest! My Creditor is a Telepathic Pervert
+-- Created: July 26, 2026 at 08:07:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1 (2 excluded)
+
+-- MAIN APPLICATION
+addappid(4754770) -- ​Hands Off the Priest! My Creditor is a Telepathic Pervert
+-- MAIN APP DEPOTS
+addappid(4754771, 1, "14bb146df497088c5f6dd795f9bc372d4594af5db16549ad5826ef4c66c28211") -- Depot 4754771
+setManifestid(4754771, "956025407486250861", 578977753)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4819980) -- Hands Off the Priest My Creditor is a Telepathic Pervert - Artbook
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Hands Off the Priest My Creditor is a Telepathic Pervert - Chibi Stickers (AppID: 4819970) - missing depot keys
+-- addappid(4819970)
+-- Hands Off the Priest My Creditor is a Telepathic Pervert - Morning Call (AppID: 4820000) - missing depot keys
+-- addappid(4820000)
