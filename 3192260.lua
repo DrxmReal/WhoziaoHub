54930e04941c195526c2db6b3a52b83e1b114656
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:52:16 +0700

    Tai moi Maid Life SS via Bot

---
+++ 3192260.lua
@@ -0,0 +1,20 @@
+-- 3192260's Lua and Manifest Created by Hubcap Manifest
+-- Maid Life SS
+-- Created: July 26, 2026 at 08:07:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3192260) -- Maid Life SS
+-- MAIN APP DEPOTS
+addappid(3192261, 1, "cff8f20a8cd34cbbcff84021d3ac12a79c4a25422ecdfc11cd94de43c1c1784b") -- Depot 3192261
+setManifestid(3192261, "201204000463432798", 565565245)
+addappid(3192262, 1, "fa6181facbc0d4630d52faaf1453bbeff7295199a858733061508b6289905b16") -- Depot 3192262
+setManifestid(3192262, "8021958150633039288", 565619335)
+addappid(3192263, 1, "1460821bd7a806825b387dc7bf7e6ae2fe4c11b3cca34e513c38a4d7616be44b") -- Depot 3192263
+setManifestid(3192263, "1207265706266324365", 571479826)
+addappid(3192264, 1, "7d7c1b698383e87f3b1aa55aca7aad7880a3e7a783c631e46723aa24cf6e6ea5") -- Depot 3192264
+setManifestid(3192264, "8240801391611318079", 552950501)
+addappid(3192265, 1, "5c926c11bb227b488dd268d31156fa915c7e564d42b7cb5e4d7666c6c3bcc65b") -- Depot 3192265
+setManifestid(3192265, "8836339054875678939", 564018499)
