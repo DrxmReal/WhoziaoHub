466b854d1b99eabc3cb444c284d2debc77dddd49
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:50:45 +0700

    Tai moi Unknown Game (2228930) via Bot

---
+++ 2228930.lua
@@ -0,0 +1,13 @@
+-- 2228930's Lua and Manifest Created by Hubcap Manifest
+-- 5SPOTS
+-- Created: July 27, 2026 at 01:19:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2228930) -- 5SPOTS
+addtoken(2228930, "3982059684362148967")
+-- MAIN APP DEPOTS
+addappid(2228931, 1, "eeadfeed87a979c654741422507d57062c8d564438cbce35217c09e29e7956be") -- Depot 2228931
+setManifestid(2228931, "8749110665918608518", 14958996)
