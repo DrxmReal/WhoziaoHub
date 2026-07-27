83350f236a30f865542928e3763641adba3f3820
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:51:18 +0700

    Tai moi Memory Adventures in the Jungle via Bot

---
+++ 2954380.lua
@@ -0,0 +1,13 @@
+-- 2954380's Lua and Manifest Created by Hubcap Manifest
+-- Memory Adventures in the Jungle
+-- Created: July 27, 2026 at 01:17:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2954380) -- Memory Adventures in the Jungle
+addtoken(2954380, "15837220413073981478")
+-- MAIN APP DEPOTS
+addappid(2954382, 1, "18840f860dbe2bf02a3237106aa7cb9c1afe2c67bc0705ad2259ca1827a0f317") -- Depot 2954382
+setManifestid(2954382, "7757712320760200245", 303843644)
