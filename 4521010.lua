8d7925999cb9bdd8a22aab8fd67ee60431e0597b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:50:01 +0700

    Tai moi holoBase via Bot

---
+++ 4521010.lua
@@ -0,0 +1,12 @@
+-- 4521010's Lua and Manifest Created by Hubcap Manifest
+-- 萌えろ!!ホロ野球
+-- Created: July 27, 2026 at 04:30:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4521010, 1, "41a3fa7975b99898d08487e1a521a8fdefbdb975a9f7c279164b6111ba4193a4") -- 萌えろ!!ホロ野球
+-- MAIN APP DEPOTS
+addappid(4521011, 1, "073cc533bbb11e29fff84344e2511c782f27d752aee9e06f81776850789d7ac3") -- Depot 4521011
+setManifestid(4521011, "1719057617155362134", 1721262603)
