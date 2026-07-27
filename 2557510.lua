6413b4266fb263c715a9c4937b76f77e45b2a75f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:49:33 +0700

    Tai moi Disciples of Varahces via Bot

---
+++ 2557510.lua
@@ -0,0 +1,14 @@
+-- 2557510's Lua and Manifest Created by Hubcap Manifest
+-- Disciples of Varahces
+-- Created: July 27, 2026 at 04:40:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2557510, 1, "182acaad95047742a68e24e5424c2104edbc45f9052b3771de7e2e661d9a2aef") -- Disciples of Varahces
+-- MAIN APP DEPOTS
+addappid(2557511, 1, "fdbcadd94dbd9f22aaaad160dc0eed2f21c733ece699dda8d203a8dcdfa3d873") -- Depot 2557511
+setManifestid(2557511, "5990410448499142347", 225174883)
+addappid(2557512, 1, "8fe46e29de1693f52c11a2eb847658c6702f40b443e47f5420b9edf3b0db74e5") -- Depot 2557512
+setManifestid(2557512, "6303698989633647000", 233120195)
