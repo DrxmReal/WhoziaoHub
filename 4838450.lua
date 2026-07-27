f77e76cfef2c9c13369e673243aabaeb510ffe15
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-27 21:51:13 +0700

    Tai moi 极乐妹土！2 via Bot

---
+++ 4838450.lua
@@ -0,0 +1,12 @@
+-- 4838450's Lua and Manifest Created by Hubcap Manifest
+-- 极乐妹土！2
+-- Created: July 27, 2026 at 01:17:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4838450) -- 极乐妹土！2
+-- MAIN APP DEPOTS
+addappid(4838451, 1, "e085bdf0d696ae1b71673c0cf0ce7ce50836e28b7b7fbab8f98b4ddbefc4f721") -- Depot 4838451
+setManifestid(4838451, "6993794073620854249", 592755479)
