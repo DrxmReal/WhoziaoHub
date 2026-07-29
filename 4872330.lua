a5e6d45eb0e0bcdf0c86a5eeceee3d5b9df13fd9
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:08:50 +0700

    Tai moi Hotel The Memory via Bot

---
+++ 4872330.lua
@@ -0,0 +1,18 @@
+-- 4872330's Lua and Manifest Created by Hubcap Manifest
+-- Hotel The Memory
+-- Created: July 29, 2026 at 04:58:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4872330) -- Hotel The Memory
+-- MAIN APP DEPOTS
+addappid(4872331, 1, "faf7c4def7911cbc3867bd6c538ec34ac0aaa98aadd028243f1b1d57954df0a3") -- Depot 4872331
+setManifestid(4872331, "1051036599956270747", 1129346866)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
