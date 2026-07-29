fc1901739066e3ca24f5fb92493d4583f8951a3b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:08:15 +0700

    Tai moi Ugoku Sushi Bar via Bot

---
+++ 3938640.lua
@@ -0,0 +1,16 @@
+-- 3938640's Lua and Manifest Created by Hubcap Manifest
+-- Ugoku Sushi Bar
+-- Created: July 29, 2026 at 11:04:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3938640) -- Ugoku Sushi Bar
+-- MAIN APP DEPOTS
+addappid(3938641, 1, "0273ab2dc8703a3a60a94a8a1145cfb3c0e9418e66088b1754e618af892711cd") -- Depot 3938641
+setManifestid(3938641, "3740657090965104930", 1565586243)
+addappid(3938642, 1, "f448fd2a949274159b33900908dc0ae874d4f31b453b49cef00aacd918357b87") -- Depot 3938642
+setManifestid(3938642, "969522721754504910", 1582215195)
+addappid(3938643, 1, "8551351d89fd3b86c4132de374facbba19d0b2084eadb0254c67df6e61d6e11e") -- Depot 3938643
+setManifestid(3938643, "6943411268311319896", 1561777907)
