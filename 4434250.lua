afbf69bf71c006e826486699a6852fe384608333
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:28 +0700

    Tai moi 拜剑·我们的冒险 via Bot

---
+++ 4434250.lua
@@ -0,0 +1,12 @@
+-- 4434250's Lua and Manifest Created by Hubcap Manifest
+-- 拜剑·我们的冒险
+-- Created: July 29, 2026 at 04:57:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4434250) -- 拜剑·我们的冒险
+-- MAIN APP DEPOTS
+addappid(4434251, 1, "5c2227f8b9b48a25be87eb6cc15f169178a4614bae0e7fd989f9c2934d0c7506") -- Depot 4434251
+setManifestid(4434251, "7548764067354073641", 3066858503)
