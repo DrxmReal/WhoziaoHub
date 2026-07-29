c149c7cd7fe739aa39dd38918e132d3d08412371
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:08:22 +0700

    Tai moi SnekromancY via Bot

---
+++ 4314770.lua
@@ -0,0 +1,16 @@
+-- 4314770's Lua and Manifest Created by Hubcap Manifest
+-- SnekromancY
+-- Created: July 29, 2026 at 08:55:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4314770) -- SnekromancY
+-- MAIN APP DEPOTS
+addappid(4314771, 1, "f224edaa7da2f82812e5e57d33174d69a73b27f2c677e76f42ab04f88e959584") -- Depot 4314771
+setManifestid(4314771, "959393000874732513", 141100404)
+addappid(4314772, 1, "329e1d1802d35b8fc05b433120c65d84f2ff2b134b7b0596c62e96292bc7c939") -- Depot 4314772
+setManifestid(4314772, "5219785387287857449", 263415661)
+addappid(4314773, 1, "a094d02542746c30461e0e1946534b1ebdca03d7a5a8ec6d7c6e1c5450c1b1c0") -- Depot 4314773
+setManifestid(4314773, "707914714747149577", 146259984)
