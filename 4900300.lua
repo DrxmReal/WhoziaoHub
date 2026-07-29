759c7089a9c8161b693fb018468548f756c004c5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:32 +0700

    Tai moi Dust Bunny Simulator via Bot

---
+++ 4900300.lua
@@ -0,0 +1,12 @@
+-- 4900300's Lua and Manifest Created by Hubcap Manifest
+-- Dust Bunny Simulator
+-- Created: July 29, 2026 at 04:57:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4900300) -- Dust Bunny Simulator
+-- MAIN APP DEPOTS
+addappid(4900301, 1, "dcb751fc8980bcd5098953c1e3948845a2d3f028a6ded11e4314c1175ef2eb7f") -- Depot 4900301
+setManifestid(4900301, "8852674391092985684", 167407336)
