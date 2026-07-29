3fd77208e9e6e5ac3c96cfefbe763c86734e4c52
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:08:04 +0700

    Tai moi Separated via Bot

---
+++ 2976110.lua
@@ -0,0 +1,18 @@
+-- 2976110's Lua and Manifest Created by Hubcap Manifest
+-- Separated
+-- Created: July 29, 2026 at 08:55:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2976110) -- Separated
+-- MAIN APP DEPOTS
+addappid(2976111, 1, "b0ea25aaac395790c55774083c8e85035609dabc044636263e3211424225b8f8") -- Depot 2976111
+setManifestid(2976111, "2547536041578933827", 8492514547)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
