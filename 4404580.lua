616d220f2d209c86c578cf67468c0edfddbe3617
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:07:57 +0700

    Tai moi MOBU via Bot

---
+++ 4404580.lua
@@ -0,0 +1,12 @@
+-- 4404580's Lua and Manifest Created by Hubcap Manifest
+-- MOBU
+-- Created: July 29, 2026 at 08:55:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4404580) -- MOBU
+-- MAIN APP DEPOTS
+addappid(4404581, 1, "1c23ba927ad56e227d4c37f0a0df9f46859106f65ff1a904ab9266794978ecef") -- Depot 4404581
+setManifestid(4404581, "1082845585327199294", 190691320)
