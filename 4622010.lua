d156eacf0bce6d5f57dd53cdc2b5fca725be6e81
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:50 +0700

    Tai moi Road Defense Racing via Bot

---
+++ 4622010.lua
@@ -0,0 +1,12 @@
+-- 4622010's Lua and Manifest Created by Hubcap Manifest
+-- Road Defense Racing
+-- Created: July 28, 2026 at 23:03:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4622010) -- Road Defense Racing
+-- MAIN APP DEPOTS
+addappid(4622011, 1, "0f0c13a805af17a5c6ab54bd165eadd9ccde85a397b1b90a6f92d14ac1d6187d") -- Depot 4622011
+setManifestid(4622011, "3934968423047856107", 794590958)
