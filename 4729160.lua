d8911bef207796d3e94b71f92f33d9dd15b938c3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:13:06 +0700

    Tai moi The Kuon Island Nursery Rhyme Murders: Case Notes via Bot

---
+++ 4729160.lua
@@ -0,0 +1,12 @@
+-- 4729160's Lua and Manifest Created by Hubcap Manifest
+-- The Kuon Island Nursery Rhyme Murders: Case Notes
+-- Created: July 28, 2026 at 11:12:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4729160) -- The Kuon Island Nursery Rhyme Murders: Case Notes
+-- MAIN APP DEPOTS
+addappid(4729161, 1, "131d58e0d479865bc312c15a4db332bd9dc5558c3cff897db03d84bb5c9cd208") -- Depot 4729161
+setManifestid(4729161, "8952235133013776941", 711081541)
