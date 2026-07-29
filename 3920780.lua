5d0a2f4cd32a4e6788ddb7b5a16dc713ccf96f79
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:01 +0700

    Tai moi Last Spaceship via Bot

---
+++ 3920780.lua
@@ -0,0 +1,18 @@
+-- 3920780's Lua and Manifest Created by Hubcap Manifest
+-- 最后的飞船
+-- Created: July 29, 2026 at 04:58:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3920780) -- 最后的飞船
+-- MAIN APP DEPOTS
+addappid(3920781, 1, "14d8876500f860dc74a39c5210744308588754aa4676ec0a7a7b8704d461919f") -- Depot 3920781
+setManifestid(3920781, "3172227827650756650", 1795775773)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
