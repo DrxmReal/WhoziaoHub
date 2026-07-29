abd8d2ea193effc58f5e68e4a7a29a8d9d48bc87
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:07:29 +0700

    Tai moi Go North via Bot

---
+++ 3041730.lua
@@ -0,0 +1,14 @@
+-- 3041730's Lua and Manifest Created by Hubcap Manifest
+-- Go North
+-- Created: July 29, 2026 at 08:55:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3041730) -- Go North
+-- MAIN APP DEPOTS
+addappid(3041731, 1, "f5fbed78c3fa193a8b83136939a54153d8715e54fde5d2e24e932b97a56835a0") -- Depot 3041731
+setManifestid(3041731, "7280404143161267501", 5535270898)
+addappid(3041732, 1, "884309d8b82a2e899566acd7f54f60ed87f0e029241cabdaa262eeea52c6d9ae") -- Depot 3041732
+setManifestid(3041732, "6835835132890897237", 5641254165)
