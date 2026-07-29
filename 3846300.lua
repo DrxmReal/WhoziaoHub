2324c8e26eeecb9d42aefcda8e058dfae2769266
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:08:58 +0700

    Tai moi MandarinMastermind via Bot

---
+++ 3846300.lua
@@ -0,0 +1,18 @@
+-- 3846300's Lua and Manifest Created by Hubcap Manifest
+-- MandarinMastermind
+-- Created: July 29, 2026 at 04:58:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3846300) -- MandarinMastermind
+-- MAIN APP DEPOTS
+addappid(3846301, 1, "d41bda521def217807a71fad338b4f04806de13b31b0c474c6632afaec75c018") -- Depot 3846301
+setManifestid(3846301, "1822060527555368542", 5723189055)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
