bc541ed85c93b4a0e9da5cba72d4f2ac9b1f3a16
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:07:21 +0700

    Tai moi Cricket Management 26 via Bot

---
+++ 4738830.lua
@@ -0,0 +1,12 @@
+-- 4738830's Lua and Manifest Created by Hubcap Manifest
+-- Cricket Management 26
+-- Created: July 29, 2026 at 08:55:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4738830) -- Cricket Management 26
+-- MAIN APP DEPOTS
+addappid(4738831, 1, "29710303a2309f5966b61751546b8741b44ab26803e5125634930e2882c67a7c") -- Depot 4738831
+setManifestid(4738831, "5091959937982873486", 480239427)
