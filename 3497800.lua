7ad2647590933e6fe6b64f4551dd300e33bbf71b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:10:03 +0700

    Tai moi Bonehold via Bot

---
+++ 3497800.lua
@@ -0,0 +1,12 @@
+-- 3497800's Lua and Manifest Created by Hubcap Manifest
+-- Bonehold
+-- Created: July 28, 2026 at 21:26:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3497800) -- Bonehold
+-- MAIN APP DEPOTS
+addappid(3497801, 1, "5c34fd0a1ee0521aa91ec1caccb47cf1f3f42c7dffa2c2ac29e54d21bed8b8b9") -- Depot 3497801
+setManifestid(3497801, "4580547595721046790", 779426941)
