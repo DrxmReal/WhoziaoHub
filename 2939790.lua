e0d68ac44b44067ce4ffa5705cb869b495131205
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:14 +0700

    Tai moi Lunarium via Bot

---
+++ 2939790.lua
@@ -0,0 +1,13 @@
+-- 2939790's Lua and Manifest Created by Hubcap Manifest
+-- Lunarium
+-- Created: July 29, 2026 at 07:26:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2939790) -- Lunarium
+addtoken(2939790, "13885911450102068332")
+-- MAIN APP DEPOTS
+addappid(2939791, 1, "b30295c5743552adf6bd73ffc9a1ccbdb81623e854ab761f45162d8af3f0e188") -- Depot 2939791
+setManifestid(2939791, "6131411377861956939", 3619551526)
