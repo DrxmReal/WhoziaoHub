a897a533e20e177f8597dc47bd7b525672e7c3e1
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:08 +0700

    Tai moi Where We Used to Play via Bot

---
+++ 4323200.lua
@@ -0,0 +1,14 @@
+-- 4323200's Lua and Manifest Created by Hubcap Manifest
+-- Where We Used to Play
+-- Created: July 29, 2026 at 04:58:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4323200) -- Where We Used to Play
+-- MAIN APP DEPOTS
+addappid(4323201, 1, "6d32772aff57d033d46ca8bdced538c503b1d6e1cfae8707ea9a48bd226cc0e2") -- Depot 4323201
+setManifestid(4323201, "3149984121563395280", 440229509)
+addappid(4323202, 1, "cdc4134d034a6c805a1e6c1cb35970fc17914738d21c53dd601219dde43ecebe") -- Depot 4323202
+setManifestid(4323202, "5093635053432627460", 1093539215)
