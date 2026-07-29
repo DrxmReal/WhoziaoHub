ed97f103bf473dcbdcbd34f0e5aa63eca1991955
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:07:15 +0700

    Tai moi Interactive Gun Range via Bot

---
+++ 2398210.lua
@@ -0,0 +1,12 @@
+-- 2398210's Lua and Manifest Created by Hubcap Manifest
+-- Interactive Gun Range
+-- Created: July 29, 2026 at 10:03:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2398210) -- Interactive Gun Range
+-- MAIN APP DEPOTS
+addappid(2398211, 1, "3960a83057c948d42db0e53510a0bcd40ee4338ab9a2da49482ef9b604f5a683") -- Depot 2398211
+setManifestid(2398211, "8184460356938459614", 871294007)
