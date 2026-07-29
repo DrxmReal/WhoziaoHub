80540f18096f18d7b438995f7c2c523b444f5e9c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:47 +0700

    Tai moi Dragon Kisser via Bot

---
+++ 4681880.lua
@@ -0,0 +1,18 @@
+-- 4681880's Lua and Manifest Created by Hubcap Manifest
+-- Dragon Kisser
+-- Created: July 28, 2026 at 23:03:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4681880) -- Dragon Kisser
+-- MAIN APP DEPOTS
+addappid(4681881, 1, "a93da00564aea3d939eeb691513e036ecef35a1ebcca70174b6e97399e13edca") -- Depot 4681881
+setManifestid(4681881, "748838653397196238", 3040657217)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
