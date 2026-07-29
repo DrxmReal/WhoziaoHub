737a03ccc1b164f7bcbfeb33fac9eed3fbc47a3c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:40 +0700

    Tai moi Abulia via Bot

---
+++ 3444230.lua
@@ -0,0 +1,12 @@
+-- 3444230's Lua and Manifest Created by Hubcap Manifest
+-- Abulia
+-- Created: July 28, 2026 at 23:15:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3444230) -- Abulia
+-- MAIN APP DEPOTS
+addappid(3444231, 1, "037cd215ee80bded2710d7eb9c4a7cc3155be7bebad3ef6cb4f6a058cbadf0db") -- Depot 3444231
+setManifestid(3444231, "2366778604531473924", 125930304)
