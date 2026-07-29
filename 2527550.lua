af9d864b07f54fa1c74ab2c97b6f9489d0423ec1
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:17 +0700

    Tai moi Roll The Bones via Bot

---
+++ 2527550.lua
@@ -0,0 +1,12 @@
+-- 2527550's Lua and Manifest Created by Hubcap Manifest
+-- Roll The Bones
+-- Created: July 29, 2026 at 04:57:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2527550) -- Roll The Bones
+-- MAIN APP DEPOTS
+addappid(2527551, 1, "c7c53cfb34c22f1780eda8cb5d5887ba31c2b8ebf7323000999c65dcdf9af0d5") -- Depot 2527551
+setManifestid(2527551, "1512048678819703293", 426442005)
