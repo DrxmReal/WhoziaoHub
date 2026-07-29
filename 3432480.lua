dd79f7f21eb45002e68f2bf30829c519b4d04abc
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:10:04 +0700

    Tai moi Shards of Order via Bot

---
+++ 3432480.lua
@@ -0,0 +1,14 @@
+-- 3432480's Lua and Manifest Created by Hubcap Manifest
+-- Shards of Order
+-- Created: July 29, 2026 at 08:27:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3432480) -- Shards of Order
+-- MAIN APP DEPOTS
+addappid(3432481, 1, "77e9ab604d07f67a797f359a9f4a8d41250ea42ab59aec3a51e4f8c844ba6b24") -- Depot 3432481
+setManifestid(3432481, "2241517734630415081", 2997916530)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4992850) -- Shards of Order - Supporters Pack
