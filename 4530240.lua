0e067e74fdb2440438ada6681cc354ce02e58c47
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:09:24 +0700

    Tai moi Dissolution of the Silent Union via Bot

---
+++ 4530240.lua
@@ -0,0 +1,14 @@
+-- 4530240's Lua and Manifest Created by Hubcap Manifest
+-- Dissolution of the Silent Union
+-- Created: July 29, 2026 at 06:56:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4530240) -- Dissolution of the Silent Union
+-- MAIN APP DEPOTS
+addappid(4530241, 1, "11d0c2592f68313f0353d63c7fd6947078026c6f85927795fd3b205ee9d6328c") -- Depot 4530241
+setManifestid(4530241, "6660803113450846936", 1016823477)
+addappid(4530242, 1, "66b9168fb52341c6ca652738be2c16ee6164df5fb0bf00896b905a20dc70c612") -- Depot 4530242
+setManifestid(4530242, "1645357860102514744", 1041190381)
