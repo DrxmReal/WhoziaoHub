ceb882dadeb5a2ab43b3b34331f5792eb7a135d3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:08:29 +0700

    Tai moi Hotel Infinity via Bot

---
+++ 4893300.lua
@@ -0,0 +1,18 @@
+-- 4893300's Lua and Manifest Created by Hubcap Manifest
+-- Hotel Infinity
+-- Created: July 29, 2026 at 08:54:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4893300) -- Hotel Infinity
+-- MAIN APP DEPOTS
+addappid(4893301, 1, "cfef84bb2ada0e379a96f94c032f6a81721d2845eaa272339afc14343a488243") -- Depot 4893301
+setManifestid(4893301, "3662948995869214049", 1229499367)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
