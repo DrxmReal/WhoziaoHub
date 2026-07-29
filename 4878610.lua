3a78bc70ccc20c72dfdae95d53b901d2d2602778
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:10:04 +0700

    Tai moi Snugfang via Bot

---
+++ 4878610.lua
@@ -0,0 +1,14 @@
+-- 4878610's Lua and Manifest Created by Hubcap Manifest
+-- Snugfang
+-- Created: July 28, 2026 at 15:46:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4878610) -- Snugfang
+-- MAIN APP DEPOTS
+addappid(4878611, 1, "72d543de8e2d0bde480a792a3f16ef177dc7069dd9127dc857871878f35831ee") -- Depot 4878611
+setManifestid(4878611, "4374072840716578291", 99893882)
+addappid(4878612, 1, "f1ac46796a1932b6d8fa7ed4c2e6e2849681eece7801e957938a9fe22f88e061") -- Depot 4878612
+setManifestid(4878612, "2902724488220149677", 83183947)
