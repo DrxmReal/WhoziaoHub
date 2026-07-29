4b4c4c5bea6d95f65eb7160a8700282389fde07f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-29 22:10:12 +0700

    Tai moi My Infamous Dungeon via Bot

---
+++ 3136320.lua
@@ -0,0 +1,12 @@
+-- 3136320's Lua and Manifest Created by Hubcap Manifest
+-- My Infamous Dungeon
+-- Created: July 29, 2026 at 07:16:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3136320) -- My Infamous Dungeon
+-- MAIN APP DEPOTS
+addappid(3136321, 1, "2043ee92fbfaf53325fdf098afb54e3285f1e2b689c40e8b7b26656aac90c081") -- Depot 3136321
+setManifestid(3136321, "7716051746532224199", 459094355)
