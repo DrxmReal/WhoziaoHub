bdfab9bdd932d48f51e631394c9d7da8eadbc15b
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-30 20:25:54 +0700

    Tai moi Mistfall Hunter via Bot

---
+++ 3282300.lua
@@ -0,0 +1,21 @@
+-- 3282300's Lua and Manifest Created by Hubcap Manifest
+-- Mistfall Hunter
+-- Created: July 30, 2026 at 00:14:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3282300) -- Mistfall Hunter
+addtoken(3282300, "18192262520444376697")
+-- MAIN APP DEPOTS
+addappid(3282301, 1, "9f7b869c77fc59d473b73126990a24d4f6f45d5d87fe6cc97e6c45e085b8b187") -- Depot 3282301
+setManifestid(3282301, "4411326363739315190", 41865974257)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4572310) -- Mistfall Hunter - Upgrade to Deluxe Edition
