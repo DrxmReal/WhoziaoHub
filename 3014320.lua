f8a17b833e054ea0e84acb3b45aeacf425ff7199
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-30 11:04:58 +0700

    Tai moi OCTOPATH TRAVELER 0 via Bot

---
+++ 3014320.lua
@@ -0,0 +1,22 @@
+-- 3014320's Lua and Manifest Created by Hubcap Manifest
+-- OCTOPATH TRAVELER 0
+-- Created: May 27, 2026 at 00:34:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3014320, 1, "f4fd915fb13264ea1005edb8f7beee8699ea3a94262534f617592669ee815ab8") -- OCTOPATH TRAVELER 0
+-- MAIN APP DEPOTS
+addappid(3014321, 1, "376135a423323c017c43031fbf0c4048cf6ea9a6ca94311cd04879ad9a043491") -- Depot 3014321
+setManifestid(3014321, "6649250304437060125", 6930504004)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3576180) -- 0 
+addappid(3718180) -- 0 
+addappid(3825480) -- OCTOPATH TRAVELER 0 Digital Deluxe Upgrade
