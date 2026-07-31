0966d22583a7c03e679486ff9a48918eabd3c798
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-07-31 13:15:50 +0700

    Tai moi Shift At Midnight via Bot

---
+++ 3722330.lua
@@ -0,0 +1,12 @@
+-- 3722330's Lua and Manifest Created by Hubcap Manifest
+-- Shift At Midnight
+-- Created: July 29, 2026 at 13:16:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3722330) -- Shift At Midnight
+-- MAIN APP DEPOTS
+addappid(3722331, 1, "241b88aeb27a250cbfff336e4e5ec6f28a60d136f4431f8bdcbf84a0506e5ed4") -- Depot 3722331
+setManifestid(3722331, "5526189819263516595", 887911876)
