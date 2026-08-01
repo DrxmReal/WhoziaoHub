0a860876cfd1f6d551f0d99f6015532c8f6953bb
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 18:27:31 +0700

    Tải mới BioBot Guide: SexySync via Bot

---
+++ 4087770.lua
@@ -0,0 +1,16 @@
+-- 4087770's Lua and Manifest Created by Hubcap Manifest
+-- BioBot Guide: SexySync
+-- Created: August 01, 2026 at 04:46:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (2 excluded)
+
+-- MAIN APPLICATION
+addappid(4087770) -- BioBot Guide: SexySync
+-- MAIN APP DEPOTS
+addappid(4087771, 1, "71e7311717ce783622f1847537116434eb0d069f87c81b3e290a6bccc165a250") -- Depot 4087771
+setManifestid(4087771, "8770501181551040163", 2479202482)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4937510) -- BioBotGuide - Chara Pack I (unreleased)
+-- addappid(4937520) -- BioBotGuide - Chara Pack II (unreleased)
