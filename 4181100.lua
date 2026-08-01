bd82249bc6798948447c097b164665cd69f774fc
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 18:02:46 +0700

    Tải mới Parking Garage Rally Circuit European Tour Soundtrack via Bot

---
+++ 4181100.lua
@@ -0,0 +1,14 @@
+-- 4181100's Lua and Manifest Created by Hubcap Manifest
+-- Parking Garage Rally Circuit European Tour Soundtrack
+-- Created: August 01, 2026 at 05:07:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4181100) -- Parking Garage Rally Circuit European Tour Soundtrack
+-- MAIN APP DEPOTS
+addappid(4181101, 1, "85397bef754402def34b9c26ad0c414e61aedfeb0a17032325c7934a28af8c81") -- Depot 4181101
+setManifestid(4181101, "7505206978718626355", 63462144)
+addappid(4181102, 1, "365ab01657759e8e6f56e745911f8b329cdf52c81d38f2cb404eee1cd3b62806") -- Depot 4181102
+setManifestid(4181102, "3397574946816082385", 419967696)
