662bf32c93ede1e98080591f06921f17d105a987
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-02 00:24:04 +0700

    Tải mới Escape from Yandere via Bot

---
+++ 4081840.lua
@@ -0,0 +1,12 @@
+-- 4081840's Lua and Manifest Created by Hubcap Manifest
+-- Escape from Yandere
+-- Created: June 03, 2026 at 15:00:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4081840, 1, "122f87e24cc72126124d07ee357b7971d7f0030558c5eaaabb641c7eafa94d9f") -- Escape from Yandere
+-- MAIN APP DEPOTS
+addappid(4081841, 1, "d7484400be27a04b6ecee36e7cedb34112d26303e4f9778b8a331a0283c8f8c9") -- Depot 4081841
+setManifestid(4081841, "8115401584334277699", 3362149261)
