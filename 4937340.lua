f18b5a4c4ff965cb38767b443e893a901b259f0f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 18:27:57 +0700

    Tải mới Boobs & Hexes via Bot

---
+++ 4937340.lua
@@ -0,0 +1,16 @@
+-- 4937340's Lua and Manifest Created by Hubcap Manifest
+-- Boobs & Hexes
+-- Created: August 01, 2026 at 04:46:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4937340) -- Boobs & Hexes
+-- MAIN APP DEPOTS
+addappid(4937342, 1, "ab2c6c2d5655f9e3c06b3f49506a0fc80933467fbefc7c3c2dc60249dd70043b") -- Depot 4937342
+setManifestid(4937342, "6223988824741061552", 516204341)
+addappid(4937343, 1, "5ad27a417f21662149b182728d4de2cc050a65ec89f08e523888ffdf7d2278d4") -- Depot 4937343
+setManifestid(4937343, "6668986744460306574", 502043891)
+addappid(4937344, 1, "1ee874f8bc61756742ab94e7677c3f47797790f70accc67c6cffce173737c4ed") -- Depot 4937344
+setManifestid(4937344, "9213085897613692510", 467777151)
