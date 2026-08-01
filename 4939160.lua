9490d85ad6bf98f0736cb8f5c2f1649a552a22a3
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 18:27:42 +0700

    Tải mới SEX Dreams at the Motel via Bot

---
+++ 4939160.lua
@@ -0,0 +1,14 @@
+-- 4939160's Lua and Manifest Created by Hubcap Manifest
+-- SEX Dreams at the Motel
+-- Created: August 01, 2026 at 04:46:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4939160) -- SEX Dreams at the Motel
+-- MAIN APP DEPOTS
+addappid(4939161, 1, "4a10b77b2a006dd7d4efa9b3af1e4ce6b41ebfdf7b01512faac077486ec5100b") -- Depot 4939161
+setManifestid(4939161, "6840975360334194711", 6501616881)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4939162) -- Depot 4939162 (empty depot)
