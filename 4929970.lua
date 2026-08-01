56fc55c76f455dc8719718b07784d68e45a70767
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 23:31:35 +0700

    Tải mới No Socks RPG via Bot

---
+++ 4929970.lua
@@ -0,0 +1,16 @@
+-- 4929970's Lua and Manifest Created by Hubcap Manifest
+-- No Socks RPG
+-- Created: July 28, 2026 at 02:45:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4929970, 1, "cf86a1cd618843c36a0cc194adfdd9922a0b8bca2ab0b2e918ec1ea7dc2cd747") -- No Socks RPG
+-- MAIN APP DEPOTS
+addappid(4929971, 1, "d52d3e344542db326e19499a99160d0bccb658fa6d1283d17d3d8a02753f552c") -- Depot 4929971
+setManifestid(4929971, "5388726005361771858", 732918857)
+addappid(4929972, 1, "ec92a70d16f7ddcc0c990efc9c9a399fe8d3ab873ba0280c13c16c844667c124") -- Depot 4929972
+setManifestid(4929972, "1675910577732776509", 1562915979)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4929973) -- Depot 4929973 (empty depot)
