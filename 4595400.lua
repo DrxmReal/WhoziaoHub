41ed917047c36e0ed81f42b5e13f74a3d5cd69a5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 18:27:05 +0700

    Tải mới Erotic Clicker via Bot

---
+++ 4595400.lua
@@ -0,0 +1,12 @@
+-- 4595400's Lua and Manifest Created by Hubcap Manifest
+-- Erotic Clicker
+-- Created: August 01, 2026 at 04:46:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4595400) -- Erotic Clicker
+-- MAIN APP DEPOTS
+addappid(4595401, 1, "e6049fa1b8bb7fde3384d861b771a48c50003f08bc6fda9995316e435fb61503") -- Depot 4595401
+setManifestid(4595401, "7105512903958460422", 4070496307)
