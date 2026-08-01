7beb55609c997b143b65879b3fab7fb57ea7042f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-01 18:27:20 +0700

    Tải mới Eliza’s Secret Potion via Bot

---
+++ 4713810.lua
@@ -0,0 +1,16 @@
+-- 4713810's Lua and Manifest Created by Hubcap Manifest
+-- イライザの秘薬
+-- Created: August 01, 2026 at 04:46:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4713810) -- イライザの秘薬
+-- MAIN APP DEPOTS
+addappid(4713811, 1, "9e5da69030c141279841c3b178311c52329ad7aa9d3f64a7c87ee0854b8a6e95") -- Depot 4713811
+setManifestid(4713811, "5541885857467774382", 741134932)
+addappid(4713812, 1, "75887c9ccf9999e0d8f698675d636500f2436223a01fce0e09e847edd0ad3c85") -- Depot 4713812
+setManifestid(4713812, "4985051746773835512", 741270310)
+addappid(4713813, 1, "6c336a772c11794faeef97328ed0e7c676ae798c55b2e34dbc8b047b00e75832") -- Depot 4713813
+setManifestid(4713813, "6537232361467507568", 778720220)
