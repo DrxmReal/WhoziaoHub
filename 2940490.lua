9ae73afa9ad9a64869b1fc2b4d64a35a4110022f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-04 19:48:52 +0700

    Tải mới Furry Aim Trainer via Bot

---
+++ 2940490.lua
@@ -0,0 +1,25 @@
+-- 2940490's Lua and Manifest Created by Hubcap Manifest
+-- Furry Aim Trainer
+-- Created: June 03, 2026 at 14:47:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 4
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2940490, 1, "e85e4bc7fc6dd2e224515203185e237bc6c4feac13ed5bcf78a1c953458cf86c") -- Furry Aim Trainer
+-- MAIN APP DEPOTS
+addappid(2940491, 1, "acb7f9e48ea2bca6750bbed6a329bafa85966f29553dc320180d270527096969") -- Depot 2940491
+setManifestid(2940491, "1727018318024752445", 921285378)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3078160) -- Furry Aim Trainer - Gay Furries
+addappid(3238760) -- Furry Aim Trainer - Futa Furries
+addappid(3323780) -- Furry Aim Trainer - Furry Feet
+addappid(3394310) -- Furry Aim Trainer - Gooning Mode
