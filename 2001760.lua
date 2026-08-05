17ed230d388765179da644f0d7d66d0645bd905c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-05 08:17:01 +0700

    Tải mới Beast of Reincarnation via Bot

---
+++ 2001760.lua
@@ -0,0 +1,21 @@
+-- 2001760's Lua and Manifest Created by Hubcap Manifest
+-- Beast of Reincarnation
+-- Created: August 04, 2026 at 05:20:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2001760) -- Beast of Reincarnation
+-- MAIN APP DEPOTS
+addappid(2001761, 1, "0d843c3fd4bba4400b829693c665e6394ad8311f3f7f6c0acf31c71eab887c0c") -- Depot 2001761
+setManifestid(2001761, "5695605622489877808", 34142175971)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4146810) -- Beast of Reincarnation Pre-Order DLC
+addappid(4146830) -- Beast of Reincarnation Deluxe DLC
