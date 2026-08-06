fb4eec5894500eecf00fd775077549992750795c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-06 08:41:03 +0700

    Tải mới Ghostly via Bot

---
+++ 3502190.lua
@@ -0,0 +1,12 @@
+-- 3502190's Lua and Manifest Created by Hubcap Manifest
+-- Ghostly
+-- Created: October 03, 2025 at 05:20:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3502190) -- Ghostly
+-- MAIN APP DEPOTS
+addappid(3502191, 1, "8af78c39c633a2dfb5cf346bdfa677bae1368330d9ab9f4186d860de64e632a5") -- Depot 3502191
+setManifestid(3502191, "2692001349629455458", 30794421)
