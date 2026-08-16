26387d746ff217112d4ee14b6337fca358b62d4c
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-16 14:04:00 +0700

    Tải mới Sex Shop Simulator: X-RAY DESIRE via Bot

---
+++ 4148670.lua
@@ -0,0 +1,18 @@
+-- 4148670's Lua and Manifest Created by Hubcap Manifest
+-- Sex Shop Simulator: X-RAY DESIRE
+-- Created: June 15, 2026 at 05:16:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4148670, 1, "16b8a77da132a04bdf7f2606d247eab0c909997215eb65f2a6b2d406583fed8b") -- Sex Shop Simulator: X-RAY DESIRE
+-- MAIN APP DEPOTS
+addappid(4148671, 1, "465e3e5940df069d4b8508ab5b77e8ec7de3c50dc67d75c1cd3aa8e9fcc3b3d3") -- Depot 4148671
+setManifestid(4148671, "1420527207514096997", 3507694100)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
