66988994361a67dc1749cb2ec35f044da5099a08
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-17 10:24:36 +0700

    Tải mới Hell Let Loose: Vietnam via Bot

---
+++ 3079210.lua
@@ -0,0 +1,16 @@
+-- 3079210's Lua and Manifest Created by Hubcap Manifest
+-- Hell Let Loose: Vietnam
+-- Created: August 16, 2026 at 07:06:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(3079210) -- Hell Let Loose: Vietnam
+-- MAIN APP DEPOTS
+addappid(3079211, 1, "f7eb2462eb25f9a05bb91c0a20e77a6f943af632b06af7d4f2e19dd912176e6c") -- Depot 3079211
+setManifestid(3079211, "6903283821410146071", 23311074317)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4447970) -- Field Supply US Army Boat Crew Uniform
+addappid(4447980) -- Field Supply NVA Boat Crew Uniform
+addappid(4448010) -- Hell Let Loose Vietnam - 2026 Field Supplies
