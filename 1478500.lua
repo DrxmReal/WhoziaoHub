adb7d7c5d86b4c5c966575c718b10e7a2179d93f
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-18 14:31:11 +0700

    Tải mới Big Walk via Bot

---
+++ 1478500.lua
@@ -0,0 +1,18 @@
+-- 1478500's Lua and Manifest Created by Hubcap Manifest
+-- Big Walk
+-- Created: August 17, 2026 at 00:13:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1478500, 1, "1174edcf23ff4903776c928407704c67bf13b9f75a546c3587fe5413055b2ca8") -- Big Walk
+-- MAIN APP DEPOTS
+addappid(1478501, 1, "f1f21164a742a9a2ce6614fc29dbe3fda4a2365c1b73e88a2738bec5cca99d53") -- Depot 1478501
+setManifestid(1478501, "2555115789188749179", 3768548636)
+addappid(1478502, 1, "595e8e546355fab56cf985a75b0c428014f60327b70dc584cba7a3bfbebe3c71") -- Depot 1478502
+setManifestid(1478502, "5926030690837510172", 3986052177)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
