c6e32cff570d66add3bba09f0ef2900160815798
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-21 22:07:02 +0700

    Tải mới Mouse X via Bot

---
+++ 4255580.lua
@@ -0,0 +1,16 @@
+-- 4255580's Lua and Manifest Created by Hubcap Manifest
+-- Mouse X
+-- Created: August 10, 2026 at 11:56:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4255580, 1, "105b378e77b9cad7b861c872aa5f4e7e85e678340727b614da2a84fe169c9290") -- Mouse X
+-- MAIN APP DEPOTS
+addappid(4255581, 1, "aa4c00e5e89a211d2168823cfc0ab5d8080fb921d46daf7a498ed6d9b1d6a6f9") -- Depot 4255581
+setManifestid(4255581, "7873686199473399735", 183670934)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
