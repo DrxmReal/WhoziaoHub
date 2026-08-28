2ea63c2c1c39db4dd75a9a2fde5f3d3e3fd21bb5
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-08-28 13:16:04 +0700

    Tai moi Waterpark Simulator via Bot

---
+++ 3293260.lua
@@ -0,0 +1,12 @@
+-- 3293260's Lua and Manifest Created by Hubcap Manifest
+-- Waterpark Simulator
+-- Created: August 21, 2026 at 12:47:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3293260) -- Waterpark Simulator
+-- MAIN APP DEPOTS
+addappid(3293261, 1, "2ae10ca475da07718687c9d3078d27cd93464eb539d53ed314d60fc261929fd7") -- Depot 3293261
+setManifestid(3293261, "3491764995452425645", 10998786587)
