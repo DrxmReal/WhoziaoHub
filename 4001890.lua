632721a05c6c67f9b66b1a4849b1a93e05e795b6
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-09-03 10:58:13 +0700

    Tải mới How to Fish via Bot

---
+++ 4001890.lua
@@ -0,0 +1,12 @@
+-- 4001890's Lua and Manifest Created by Hubcap Manifest
+-- How to Fish
+-- Created: September 01, 2026 at 16:09:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4001890) -- How to Fish
+-- MAIN APP DEPOTS
+addappid(4001891, 1, "b5c8ad37740a8db6d370a6934bf203cfaeb3343003e6277ffc96c63f4df2b7ca") -- Depot 4001891
+setManifestid(4001891, "8322781817822782512", 643047696)
