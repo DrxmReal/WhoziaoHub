cc61dacf57fc057e31fdb4b36f4856cf2a60657d
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-09-03 12:11:38 +0700

    Tải mới The Blood of Dawnwalker via Bot

---
+++ 3751260.lua
@@ -0,0 +1,21 @@
+-- 3751260's Lua and Manifest Created by Hubcap Manifest
+-- The Blood of Dawnwalker
+-- Created: September 02, 2026 at 18:21:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3751260, 1, "bfb8982d4aeb1b3b81978c18fb93b66cbc707cf9530ab92769d00c50e90c4031") -- The Blood of Dawnwalker
+-- MAIN APP DEPOTS
+addappid(3751261, 1, "fe44cc16911c3d5dabb2b862a70f1b8b58ee79e1da1f47b8bf03af487c17e8fc") -- Depot 3751261
+setManifestid(3751261, "116071877880222957", 58350822521)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4417540) -- The Blood of Dawnwalker - Sangoran Wayfarers Armor Set
+addappid(4417550) -- The Blood of Dawnwalker - Eclipse Edition Content
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3751262) -- Depot 3751262 (empty depot)
