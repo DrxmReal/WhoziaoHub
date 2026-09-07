b7c85f77b38e4749a7380ca35bf7e7146cc9bd90
Drxm <88696837+DrxmReal@users.noreply.github.com>
2026-09-07 16:28:10 +0700

    Tải mới Mimic Party via Bot

---
+++ 5053820.lua
@@ -0,0 +1,14 @@
+-- 5053820's Lua and Manifest Created by Hubcap Manifest
+-- Mimic Party
+-- Created: September 07, 2026 at 04:49:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(5053820, 1, "3687a910eb76c9e3eccddcb2ce6cc97c4bc5667ef8e20e668cbedc150283773a") -- Mimic Party
+-- MAIN APP DEPOTS
+addappid(5053821, 1, "3bf0ccc9e3c7c0ff61adcadc28e45680676fe6f9a82c9c4ad55c417c532c35e6") -- Depot 5053821
+setManifestid(5053821, "8143648887960611233", 641030324)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(5168110) -- All Characters
